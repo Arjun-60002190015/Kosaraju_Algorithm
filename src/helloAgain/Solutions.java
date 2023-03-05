13e84a4058dd831797efa4027d745b5c9dcb0754
dell <arjunpathak164@gmail.com>
2023-03-05 21:18:13 +0530

    hello

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -1,31 +1,51 @@
 package helloAgain;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Solutions {
-    public boolean canReach(int[] arr, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        while(!q.isEmpty()){
-            start = q.poll();
-            if(arr[start]==0)
-                return true;
-            if(arr[start]<0)
-                continue;
-            if(arr[start] + start<arr.length)
-                q.add(arr[start] + start);
-            if(start - arr[start]>=0)
-                q.add(start - arr[start]);
-
-            arr[start] *= -1;
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        if(n==1) return 0;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int step = 0;
+        for(int i = 0;i<n;i++){
+            map.computeIfAbsent(arr[i], v -> new ArrayList<>()).add(i);
         }
-        return false;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        while(!q.isEmpty()){
+            step++;
+            int size = q.size();
+            for(int i = 0;i<size;i++){
+                int j = q.poll();
+                if(j-1>=0 && map.containsKey(arr[j-1])){
+                    q.add(j-1);
+                }
+                if(j+1<n && map.containsKey(arr[j+1])){
+                    if(j+1==n-1)
+                        return step;
+                    q.offer(j+1);
+                }
+                if(map.containsKey(arr[j])){
+                    for(int k:map.get(arr[j])){
+                        if(k!=j){
+                            if(k==n-1) return step;
+                            q.offer(k);
+                        }
+                    }
+                }
+                map.remove(arr[j]);
+            }
+        }
+        return step;
+
 
     }
 
 
     public static void main(String[] args){
+        int[] arr = {4,2,3,0,3,1,2};
+        //System.out.println(canReach(arr, 5));
 
     }
 
