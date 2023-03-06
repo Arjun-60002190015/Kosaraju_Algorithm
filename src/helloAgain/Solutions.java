9af5f77f8f15ea8e75769f8f969c3ccf194042c2
dell <arjunpathak164@gmail.com>
2023-03-06 10:09:09 +0530

    hello

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -2,50 +2,29 @@ package helloAgain;
 
 import java.util.*;
 
+
 public class Solutions {
-    public int minJumps(int[] arr) {
-        int n = arr.length;
-        if(n==1) return 0;
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        int step = 0;
-        for(int i = 0;i<n;i++){
-            map.computeIfAbsent(arr[i], v -> new ArrayList<>()).add(i);
+    public static int findKthPositive(int[] arr, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i:arr){
+            set.add(i);
         }
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(0);
-        while(!q.isEmpty()){
-            step++;
-            int size = q.size();
-            for(int i = 0;i<size;i++){
-                int j = q.poll();
-                if(j-1>=0 && map.containsKey(arr[j-1])){
-                    q.add(j-1);
-                }
-                if(j+1<n && map.containsKey(arr[j+1])){
-                    if(j+1==n-1)
-                        return step;
-                    q.offer(j+1);
-                }
-                if(map.containsKey(arr[j])){
-                    for(int k:map.get(arr[j])){
-                        if(k!=j){
-                            if(k==n-1) return step;
-                            q.offer(k);
-                        }
-                    }
-                }
-                map.remove(arr[j]);
+        int j = 0;
+        int num = 0;
+        for(int i = 1;j<k;i++){
+            if(!set.contains(i)){
+                num = i;
+                j++;
             }
         }
-        return step;
-
+        return num;
 
     }
 
 
     public static void main(String[] args){
-        int[] arr = {4,2,3,0,3,1,2};
-        //System.out.println(canReach(arr, 5));
+        int[] arr = {2,3,4, 7, 11};
+        System.out.println(findKthPositive(arr, 5));
 
     }
 
