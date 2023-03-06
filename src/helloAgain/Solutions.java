67f759267f0975125238c09f81ff9c4a5751ef00
dell <arjunpathak164@gmail.com>
2023-03-06 23:14:35 +0530

    hello

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -5,11 +5,17 @@ import java.util.*;
 
 
 public class Solutions {
-    public boolean squareIsWhite(String coordinates) {
-
-        int num1 = coordinates.charAt(0) - '0';
-        int num2 = coordinates.charAt(1);
-        return (num1+num2)%2==0?false:true;
+    public int sumOfUnique(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        for(int i:nums){
+            map.put(i ,map.getOrDefault(i, 0)+1);
+        }
+        for(int i = 0;i< nums.length;i++){
+            if(map.get(nums[i])==1)
+                sum += nums[i];
+        }
+        return sum;
 
 
     }
