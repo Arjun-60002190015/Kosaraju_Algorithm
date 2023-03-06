a0e1fb71605f56ff23fc99eddf7282d07f648151
dell <arjunpathak164@gmail.com>
2023-03-06 22:56:47 +0530

    hello

--- src/helloAgain/mcm.java
+++ src/helloAgain/mcm.java
@@ -1,38 +1,53 @@
 package helloAgain;
 
-//Palindrome Partitioning recursive
+//Palindrome Partitioning memoized
 
 public class mcm {
+    static int[][] dp = new int[2001][2001];
 
-    public static boolean isP(String s, int i, int j){
-        if(s.charAt(i)!=s.charAt(j))
-            return false;
-        i++;
-        j--;
 
-        return true;
-    }
-    public static int minCut(String s) {
-        return helper(s, 0, s.length() - 1);
+    public int minCut(String str) {
+        for(int i = 0;i< dp.length;i++){
+            for(int j = 0;j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+        }
+        return helper(str, 0, str.length()-1);
 
     }
 
     public static int helper(String s, int i, int j){
         if(i>=j)
             return 0;
-        int min = Integer.MAX_VALUE;
         if(isP(s, i, j))
             return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        dp[i][j] = Integer.MAX_VALUE;
         for(int k = i;k<j;k++){
-            int solve = helper(s, i, k) + helper(s, k+1, j) + 1;
-            min = Math.min(min, solve);
+            if(isP(s, i, k)) {
+                int solve = Math.min(dp[i][j], helper(s, i, k) + helper(s, k + 1, j) + 1);
+                dp[i][j] = solve;
+            }
         }
-        return min;
-
-
-
+        return dp[i][j];
 
     }
+
+    public static boolean isP(String s, int i, int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+
+
 
     public static void main(String[] args){
         String s = "aab";
