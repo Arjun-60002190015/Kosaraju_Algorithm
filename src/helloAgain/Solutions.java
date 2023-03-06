ba3f359d167ba6db5510d0d2963488b158d80dba
dell <arjunpathak164@gmail.com>
2023-03-06 23:06:00 +0530

    hello

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -1,31 +1,23 @@
 package helloAgain;
 
 import java.util.*;
-//Solution to Jump Game 4(leetcode) 😎😎😎😎
+
 
 
 public class Solutions {
-    public static int findKthPositive(int[] arr, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i:arr){
-            set.add(i);
-        }
-        int j = 0;
-        int num = 0;
-        for(int i = 1;j<k;i++){
-            if(!set.contains(i)){
-                num = i;
-                j++;
-            }
-        }
-        return num;
+    public boolean squareIsWhite(String coordinates) {
+
+        int num1 = coordinates.charAt(0) - '0';
+        int num2 = coordinates.charAt(1);
+        return (num1+num2)%2==0?false:true;
+
 
     }
 
 
     public static void main(String[] args){
-        int[] arr = {2,3,4, 7, 11};
-        System.out.println(findKthPositive(arr, 5));
+        int[] arr = {1,5,2, 4, 1};
+        //System.out.println((arr));
 
     }
 
