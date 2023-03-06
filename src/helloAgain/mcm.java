45db19525fe05cdc32c23805a342c3c01a1a3736
dell <arjunpathak164@gmail.com>
2023-03-06 20:54:46 +0530

    hello

--- src/helloAgain/mcm.java
+++ src/helloAgain/mcm.java
@@ -1,37 +1,41 @@
 package helloAgain;
 
-//MCM Memoized
+//Palindrome Partitioning recursive
 
 public class mcm {
-    static int dp[][] = new int[1001][1001];
-    public static int solve(int[] arr){
-        for(int i = 0;i<dp.length;i++){
-            for(int j = 0;j<dp[0].length;j++){
-                dp[i][j] = -1;
-            }
-        }
-        return helper(arr, 1, arr.length-1);
 
+    public static boolean isP(String s, int i, int j){
+        if(s.charAt(i)!=s.charAt(j))
+            return false;
+        i++;
+        j--;
+
+        return true;
+    }
+    public static int minCut(String s) {
+        return helper(s, 0, s.length() - 1);
 
     }
-    public static int helper(int[] arr, int i, int j){
+
+    public static int helper(String s, int i, int j){
         if(i>=j)
             return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
         int min = Integer.MAX_VALUE;
-        for(int k = i;k<=j-1;k++){
-            int temp = helper(arr, i, k) + helper(arr, k+1, j)
-                     + arr[i-1] * arr[k] * arr[j];
-            if(temp<min)
-                min = temp;
+        if(isP(s, i, j))
+            return 0;
+        for(int k = i;k<j;k++){
+            int solve = helper(s, i, k) + helper(s, k+1, j) + 1;
+            min = Math.min(min, solve);
         }
-        return dp[i][j] = min;
+        return min;
+
+
+
 
     }
 
     public static void main(String[] args){
-        int[] arr = {40, 20, 30, 10, 30};
-        System.out.println(solve(arr));
+        String s = "aab";
+        System.out.println(minCut(s));
     }
 }
