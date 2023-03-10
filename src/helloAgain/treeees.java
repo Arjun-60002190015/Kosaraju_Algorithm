0a465b6c607dee9d4c59045308e83aac706fcca0
dell <arjunpathak164@gmail.com>
2023-03-10 15:33:33 +0530

    hi

--- src/helloAgain/treeees.java
+++ src/helloAgain/treeees.java
@@ -1,6 +1,10 @@
 package helloAgain;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 public class treeees {
@@ -27,17 +31,56 @@ public class treeees {
           next = null;
       }
     }
-    public ListNode removeElements(ListNode head, int val) {
+    public boolean isEvenOddTree(TreeNode root) {
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.addLast(root);
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        int level = 0;
+        while(!q.isEmpty()){
+            ArrayList<Integer> temp = new ArrayList<>();
+            int size = temp.size();
+            while(size-- >0){
+                TreeNode curr = q.removeFirst();
+                if(level%2==0){
+                    if(curr.val%2==0)
+                        return false;
+                }
+                if(level%2!=0){
+                    if(curr.val%2!=0)
+                        return false;
+                }
+                temp.add(curr.val);
+                if(curr.left!=null)
+                    q.addLast(curr.left);
+                if(curr.right!=null)
+                    q.addLast(curr.right);
 
-        if(head==null)
-            return null;
-        ListNode pointer = head;
-        while(pointer.next!=null){
-            if(pointer.next.val==val)pointer.next = pointer.next.next;
-            else pointer = pointer.next;
+            }
+            res.add(temp);
+            level++;
         }
-        return head.val==val?head.next:head;
+        int l = 0;
+        for(ArrayList<Integer> list:res){
+            if(l%2==0){
+                for(int i = 1;i< list.size();i++){
+                    if(list.get(i-1)>=list.get(i))
+                        return false;
+                }
+            }else{
+                for(int i = 1;i< list.size();i++){
+                    if(list.get(i-1)<=list.get(i))
+                        return false;
+                }
+            }
+            l++;
+        }
+        return true;
 
+    }
+
+
+
+    public static void main(String[] args){
 
     }
 
