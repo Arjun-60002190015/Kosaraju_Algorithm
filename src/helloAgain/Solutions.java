a5d73313a3ee7a7b6154a9f14e3f64961d5a8a08
dell <arjunpathak164@gmail.com>
2023-03-11 15:51:19 +0530

    hello

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -1,23 +1,57 @@
 package helloAgain;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 
 
 public class Solutions {
 
-    public int maximumGroups(int[] grades) {
-        int count = 1;
-        int i = 1;
-        int helper = 2;
-        while(i<grades.length){
-            i += helper;
-            helper++;
-            if(i<=grades.length)
-                count++;
+    public int kthSmallestBruteForce(int[][] matrix, int k) {
+        int n = matrix.length;
+        int index = 0;
+        int[] arr = new int[n*n];
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                arr[index] = matrix[i][j];
+                index++;
+            }
+        }
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int min = matrix[0][0];
+        int max = matrix[n-1][n-1];
+        while(min!=max){
+            int mid = min - (max - min)/2;
+            int count = helper(matrix, min);
+            if(count<k){
+                min = mid + 1;
+            }else{
+                max = mid;
+            }
+        }
+        return min;
+
+    }
+
+    public int helper(int[][] matrix, int mid){
+        int count = 0;
+        int row = 0;
+        int col = matrix.length-1;
+
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col]<=mid){
+                count += col +1;
+                row++;
+            }else{
+                col--;
+            }
         }
         return count;
-
     }
 
 
@@ -26,7 +60,9 @@ public class Solutions {
 
 
 
-    public static void main(String[] args){
+
+
+        public static void main(String[] args){
         char[] c = {'c', 'f', 'j'};
         int n = 5;
         //System.out.println(nextGreatestLetter(c, 'a'));
