4fd6033a499a9c0f514886efd9a8e1e3ad16b015
dell <arjunpathak164@gmail.com>
2023-03-13 15:57:48 +0530

    hey there

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -7,14 +7,18 @@ import java.util.*;
 
 public class Solutions {
 
-    public static int minOperations(int n) {
-        int count = 0;
-
-        for(int i = 1;i<n;i+=2){
-            count += n - i;
+    public static int BinarySearch(int[] n, int target) {
+        int start = 0;
+        int end = n.length - 1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(n[mid]>target){
+                start = mid + 1;
+            }else if(n[mid]<target){
+                end = mid - 1;
+            }
         }
-        return count;
-
+        return start;
     }
 
 
@@ -22,8 +26,8 @@ public class Solutions {
 
     public static void main(String[] args){
 
-        int[] nums = {-2};
-        System.out.println(minOperations(3));
+        int[] nums = {20, 17, 15, 13, 11, 8, 7, 4, 3};
+        System.out.println(BinarySearch(nums, 3));
 
     }
 
