9e780e1b872210b75287bfc900b45e5d17d1c021
dell <arjunpathak164@gmail.com>
2023-03-19 14:15:45 +0530

    hilo

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,14 +8,14 @@ import java.util.*;
 
 
 public class Solutions {
-    public static int maxflipOne(int[] nums){
+    public int longestOnes(int[] nums, int k) {
         int ans = 0;
         int j = -1;
         int count = 0;
         for(int i = 0;i<nums.length;i++){
             if(nums[i]==0)
                 count++;
-            while(count>1){
+            while(count>k){
                 j++;
                 if(nums[j]==0)
                     count--;
@@ -25,6 +25,7 @@ public class Solutions {
                 ans = len;
         }
         return ans;
+
     }
 
 
