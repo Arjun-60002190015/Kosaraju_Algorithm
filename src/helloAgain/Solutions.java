2ef060a38ef794310df8b5e348a6e65e75c6e833
dell <arjunpathak164@gmail.com>
2023-03-20 18:17:36 +0530

    hilo

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,22 +8,26 @@ import java.util.*;
 
 
 public class Solutions {
-    int min = Integer.MAX_VALUE;
-    public int getMinDistance(int[] nums, int target, int start) {
-        int min = Integer.MAX_VALUE;
-        int index = -1;
-        for(int i = 0;i< nums.length;i++){
-            if(nums[i]==target){
-                index = i;
-                min = Math.min(min, Math.abs(index - start));
+    public int[] numberOfLines(int[] widths, String s) {
+        int count = 0;
+        char[] ch = s.toCharArray();
+        int[] res = new int[2];
+        for(int i = 0;i< ch.length;i++){
+            if(count + widths[s.charAt(i) - 'a']>100){
+                count = 0;
+                res[0]++;
             }
+            count += widths[s.charAt(i) - 'a'];
         }
-        return min;
+        res[0]++;
+        res[1] = count;
+        return res;
     }
 
 
 
-    public static void main(String[] args){
+
+        public static void main(String[] args){
 
         int[] nums = {-5, 8, -14, 2, 4, 12};
         //System.out.println(maxSub(nums, -5));
