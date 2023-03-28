16a6834fa0e851831efd38771858f825206ab7c6
dell <arjunpathak164@gmail.com>
2023-03-28 21:06:58 +0530

    qwerty

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,50 +8,25 @@ import java.util.*;
 
 
 public class Solutions {
-    static int min = Integer.MAX_VALUE;
-    public static int minPathSumRecursive(int[][] grid) {
-        return helper(grid, grid.length - 1, grid[0].length - 1);
+
+
+    public boolean isMonotonic(int[] nums) {
+        int forw = 0;
+        int back = 0;
+        for(int i = 1;i< nums.length;i++){
+            if(nums[i]>=nums[i-1])
+                forw++;
+            if(nums[i]<=nums[i-1])
+                back++;
+        }
+        return forw== nums.length-1 || back== nums.length-1;
 
     }
-
-    public static int helper(int[][] grid, int i, int j){
-        if(i<0 || j<0)
-            return Integer.MAX_VALUE;
-        if(i==0 && j==0)
-            return grid[i][j];
-        int left = helper(grid, i-1, j);
-        int right = helper(grid, i, j-1);
-        return grid[i][j] + Math.min(left, right);
-
-
-    }
-
-    public static int minPathSum(int[][] grid){
-        int[][] dp = new int[grid.length][grid[0].length];
-        dp[0][0] = grid[0][0];
-        for(int i  = 1;i<grid[0].length;i++){
-            dp[0][i] = dp[0][i-1] + grid[0][i];
-        }
-        for(int i = 1;i<grid.length;i++){
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-        }
-        for(int i = 0;i<grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        return dp[grid.length - 1][grid[0].length - 1];
-    }
-
-
-
-
-
 
 
     public static void main(String[] args){
         int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
-        System.out.println(minPathSum(grid));
+        //System.out.println(minPathSum(grid));
 
     }
 
