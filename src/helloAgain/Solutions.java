dce9bb1eacaf8c2d1471666de0eaf85d137a4675
dell <arjunpathak164@gmail.com>
2023-03-29 13:35:54 +0530

    qwerty

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -10,26 +10,31 @@ import java.util.*;
 public class Solutions {
 
 
-    public static int maxSatisfaction(int[] satisfaction) {
+    public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
-        int sum = 0;
-        for(int i = 0;i<satisfaction.length;i++){
-            int index = 1;
-            int temp = 0;
-            for(int j = i;j<satisfaction.length;j++){
-                temp += satisfaction[j] * index;
-                index++;
-            }
-            sum = Math.max(temp, sum);
+        int n = satisfaction.length;
+        int[][] dp = new int[n+1][n+1];
+        for(int[] row:dp){
+            Arrays.fill(row, Integer.MIN_VALUE);
         }
-        return sum;
+        return helper(satisfaction, 0, 1, dp);
+    }
 
+    public int helper(int[] nums, int index, int time, int[][] dp){
+        if(index==nums.length)
+            return 0;
+        if(dp[index][time]!=Integer.MIN_VALUE){
+            return dp[index][time];
+        }
+        int dont = helper(nums, index+1, time, dp);
+        int take = helper(nums, index+1, time+1, dp) + nums[index] * time;
+        return dp[index][time] = Math.max(dont, take);
     }
 
 
     public static void main(String[] args){
         int[] nums = {-1, -8, 0, 5, -9};
-        System.out.println(maxSatisfaction(nums));
+        //System.out.println(maxSatisfaction(nums));
 
     }
 
