298f683eb444cbc6179ce58623b413c9b3fac535
dell <arjunpathak164@gmail.com>
2023-04-02 13:05:02 +0530

    qwerty

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,36 +8,32 @@ import java.util.*;
 
 
 public class Solutions {
-    public static List<List<Integer>> findMatrix(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i:nums){
-            map.put(i, map.getOrDefault(i, 0)+1);
-        }
-        while(!map.isEmpty()){
-            List<Integer> temp = new ArrayList<>();
-            for(Integer key: map.keySet()){
-                if(map.get(key)>0)
-                    temp.add(key);
-                map.put(key, map.get(key) - 1);
+    public static int findTheLongestBalancedSubstring(String s) {
+        //int start = 0;
+        int res = 0;
+        int count = 0;
+        int end = 0;
+        char[] ch = s.toCharArray();
+
+        while(end<ch.length){
+            if(ch[end]==0) {
+                count++;
+
+
+            }else {
+                count--;
             }
-            if(temp.size()==0)
-                break;
-            res.add(new ArrayList<>(temp));
+            res = Math.max(count, res);
+            end++;
+
         }
-        return res;
+        return res*2;
 
     }
 
-
-
-
-
-
     public static void main(String[] args){
-        int[] arr = {2, 5, 5, 7};
-        int[] nums = {1, 3, 4, 1, 2, 3, 1};
-        System.out.println(findMatrix(nums));
+        String s = "01000111";
+        System.out.println(findTheLongestBalancedSubstring(s));
 
     }
 
