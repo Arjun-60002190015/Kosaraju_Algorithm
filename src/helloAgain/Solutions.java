49a223993f909fbf2e08ee74b689b495562af34d
dell <arjunpathak164@gmail.com>
2023-04-03 17:44:41 +0530

    hey

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,23 +8,31 @@ import java.util.*;
 
 
 public class Solutions {
-    public static int numRescueBoats(int[] people, int limit) {
-        int count = 0;
-        Arrays.sort(people);
-        int start = 0;
-        int end = people.length-1;
-        while(start<=end) {
-            if (people[end] + people[start] <= limit){
-                start++;
-                end--;
-            }
-            else{
-                end--;
-            }
-            count++;
+    public int miceAndCheese(int[] reward1, int[] reward2, int k) {
+        int res = 0;
+        int[] rev = new int[reward1.length];
+        for(int i = 0;i< rev.length;i++){
+            rev[i] = reward1[i] - reward2[i];
+            res += reward2[i];
         }
-        return count;
+        Arrays.sort(rev);
+        reverse(rev);
 
+        for(int i = 0;i<k;i++){
+            res += rev[i];
+        }
+        return res;
+    }
+    public void reverse(int[] a){
+        int i = 0;
+        int j = a.length-1;
+        while(i<j){
+            int t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+            i++;
+            j--;
+        }
     }
 
 
@@ -32,7 +40,7 @@ public class Solutions {
     public static void main(String[] args){
         int[] people = {3, 5, 3, 4};
         int[] potions = {1, 2, 3, 4, 5};
-        System.out.println(numRescueBoats(people, 5));
+        //System.out.println(numRescueBoats(people, 5));
 
     }
 
