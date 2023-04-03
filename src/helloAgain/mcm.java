b53bd5e4737991b1080bebc7b967233460a00885
dell <arjunpathak164@gmail.com>
2023-04-03 20:35:03 +0530

    hey

--- src/helloAgain/mcm.java
+++ src/helloAgain/mcm.java
@@ -5,6 +5,19 @@ package helloAgain;
 import java.util.Arrays;
 
 public class mcm {
+    public int superEggDrop(int k, int n) {
+        if(n==0 || n==1)
+            return n;
+        if(k==1)
+            return n;
+        int min = Integer.MAX_VALUE;
+        for(int i = 1;i<=k;i++){
+            int temp = 1+ Math.max(superEggDrop(k - 1, i-1), superEggDrop(k, n-i));
+            min = Math.min(min, temp);
+        }
+        return min;
+
+    }
 
 
 
