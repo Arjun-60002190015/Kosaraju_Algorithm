7b860b4f50bbbb5db7be87d24f507622a2536509
dell <arjunpathak164@gmail.com>
2023-04-03 16:21:42 +0530

    hey

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,38 +8,31 @@ import java.util.*;
 
 
 public class Solutions {
-    public static int[] successfulPairs(int[] spells, int[] potions, long success) {
-        Arrays.sort(potions);
-        for(int i = 0;i< spells.length;i++){
-            long curr = spells[i];
-            spells[i] = binaryS(spells[i], potions, success);
-        }
-        return spells;
-
-
-    }
-
-    public static int binaryS(int i, int[] potions, long success){
+    public static int numRescueBoats(int[] people, int limit) {
+        int count = 0;
+        Arrays.sort(people);
         int start = 0;
-        int end = potions.length-1;
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(potions[mid]*i>= success){
-                end = mid - 1;
+        int end = people.length-1;
+        while(start<=end) {
+            if (people[end] + people[start] <= limit){
+                start++;
+                end--;
             }
             else{
-                start = mid+1;
+                end--;
             }
+            count++;
         }
-        return potions.length - start;
+        return count;
+
     }
 
 
 
     public static void main(String[] args){
-        int[] spells = {5, 1, 3};
+        int[] people = {3, 5, 3, 4};
         int[] potions = {1, 2, 3, 4, 5};
-        System.out.println(successfulPairs(spells, potions, 7));
+        System.out.println(numRescueBoats(people, 5));
 
     }
 
