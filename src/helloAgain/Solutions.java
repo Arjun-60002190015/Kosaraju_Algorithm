f09642954a205ba15d99afbff258265b3075caf1
dell <arjunpathak164@gmail.com>
2023-04-04 22:45:56 +0530

    hyy

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,25 +8,67 @@ import java.util.*;
 
 
 public class Solutions {
-    public int peakIndexInMountainArray(int[] arr) {
+    public static int bs(int target, int[] nums) {
+        int PeakIndex = getPeak(nums);
+        int Lindex = bslow(0, PeakIndex, nums,  target);
+        int Hindex = bshigh(PeakIndex+1, nums.length-1, nums, target);
+
+        if(Lindex==-1 && Hindex==-1)
+            return -1;
+        if(Lindex==-1)
+            return Hindex;
+        return Lindex;
+
+
+
+    }
+
+    public static int getPeak(int[] nums){
         int low = 0;
-        int high = arr.length-1;
+        int high = nums.length-1;
         while(low<high){
             int mid = low + (high - low)/2;
-            if(arr[mid]<arr[mid+1]){
+            if(nums[mid]<nums[mid+1]){
                 low = mid+1;
-            }else if(arr[mid]>arr[mid+1]){
-                high  = mid;
+            }else if(nums[mid]>nums[mid+1]){
+                high = mid;
             }
         }
         return low;
+    }
 
+    public static int bslow(int low, int high, int[] nums, int key){
+        int index = -1;
+        while(low<high){
+            int mid = low + (high - low)/2;
+            if(nums[mid]<key){
+                low = mid+1;
+            }else if(nums[mid]>key){
+                high = mid;
+            }else index = mid;
+        }
+        return index;
+    }
+
+    public static int bshigh(int high, int low, int[] nums, int key){
+        int index = -1;
+        while(high<low){
+            int mid = high + (low - high)/2;
+            if(nums[mid]<key){
+                high = mid+1;
+            }else if(nums[mid]>key){
+                low = mid;
+            }else index = mid;
+        }
+        return index;
     }
 
 
     public static void main(String[] args){
-        String num = "52";
-        //System.out.println(largestOddNumber(num));
+        int[] nums = {1, 3, 8, 12, 4, 2};
+        int target = 4;
+
+        System.out.println(bs(target, nums));
 
     }
 
