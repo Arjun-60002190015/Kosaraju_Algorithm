6e52f88ff8753d78c872a5a8917805028b742f6e
dell <arjunpathak164@gmail.com>
2023-04-05 15:16:18 +0530

    hyy

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,67 +8,44 @@ import java.util.*;
 
 
 public class Solutions {
-    public static int bs(int target, int[] nums) {
-        int PeakIndex = getPeak(nums);
-        int Lindex = bslow(0, PeakIndex, nums,  target);
-        int Hindex = bshigh(PeakIndex+1, nums.length-1, nums, target);
-
-        if(Lindex==-1 && Hindex==-1)
-            return -1;
-        if(Lindex==-1)
-            return Hindex;
-        return Lindex;
-
-
-
-    }
-
-    public static int getPeak(int[] nums){
-        int low = 0;
-        int high = nums.length-1;
-        while(low<high){
-            int mid = low + (high - low)/2;
-            if(nums[mid]<nums[mid+1]){
-                low = mid+1;
-            }else if(nums[mid]>nums[mid+1]){
-                high = mid;
+    public static int maximumCandies(int[] candies, long k) {
+        int start = 1;
+        int end = 1;
+        for(int i:candies){
+            end = Math.max(end, i);
+        }
+        while(start<end){
+            int mid = start + (end - start)/2;
+            if(getSum(candies, k, mid)){
+                start = mid+1;
+            }else{
+                end = mid - 1;
             }
         }
-        return low;
+        return getSum(candies, k, start)?start:start - 1;
+
+
     }
 
-    public static int bslow(int low, int high, int[] nums, int key){
-        int index = -1;
-        while(low<high){
-            int mid = low + (high - low)/2;
-            if(nums[mid]<key){
-                low = mid+1;
-            }else if(nums[mid]>key){
-                high = mid;
-            }else index = mid;
+    public static boolean getSum(int[] candies, long k, int mid){
+        int total = 0;
+        for(int i:candies){
+            total += i/mid;
         }
-        return index;
+        if(total>=k)
+            return true;
+        return false;
+
     }
 
-    public static int bshigh(int high, int low, int[] nums, int key){
-        int index = -1;
-        while(high<low){
-            int mid = high + (low - high)/2;
-            if(nums[mid]<key){
-                high = mid+1;
-            }else if(nums[mid]>key){
-                low = mid;
-            }else index = mid;
-        }
-        return index;
-    }
+
 
 
     public static void main(String[] args){
-        int[] nums = {1, 3, 8, 12, 4, 2};
+        int[] nums = {5, 8, 6};
         int target = 4;
 
-        System.out.println(bs(target, nums));
+        System.out.println(maximumCandies(nums, 3));
 
     }
 
