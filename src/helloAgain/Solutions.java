97f9076358e3cccb66bf089bd47ba30f59141f86
dell <arjunpathak164@gmail.com>
2023-04-06 11:32:02 +0530

    hyy

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,23 +8,32 @@ import java.util.*;
 
 
 public class Solutions {
-    public static boolean searchMatrix(int[][] matrix, int target) {
-
-        int i = 0;
-        int j = matrix[0].length-1;
-        while(i< matrix.length && j>=0){
-            if(matrix[i][j]==target)
-                return true;
-            if(matrix[i][j]<target){
-                i++;
+    public int closedIsland(int[][] grid) {
+        int count = 0;
+        for(int i = 0;i< grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
+                if(grid[i][j]==0){
+                    if(dfs(grid, i, j))
+                        count++;
+                }
             }
-            else {
-                j--;
-            }
-
         }
-        return false;
+        return count;
 
+
+    }
+
+    public static boolean dfs(int[][] grid, int i, int j){
+        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length)
+            return false;
+        if(grid[i][j]==1 || grid[i][j]==2)
+            return true;
+        grid[i][j] = 2;
+        boolean up = dfs(grid, i-1, j);
+        boolean down = dfs(grid, i+1, j);
+        boolean left = dfs(grid, i, j-1);
+        boolean right = dfs(grid, i, j+1);
+        return up&&down&&right&&left;
 
     }
 
