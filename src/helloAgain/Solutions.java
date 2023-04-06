38b05004fbded8583cb979a8340d4bc32500a05a
dell <arjunpathak164@gmail.com>
2023-04-06 11:54:41 +0530

    hyy

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,32 +8,17 @@ import java.util.*;
 
 
 public class Solutions {
-    public int closedIsland(int[][] grid) {
-        int count = 0;
-        for(int i = 0;i< grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                if(grid[i][j]==0){
-                    if(dfs(grid, i, j))
-                        count++;
-                }
-            }
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i:nums){
+            map.put(i, map.getOrDefault(i, 0)+1);
         }
-        return count;
-
-
-    }
-
-    public static boolean dfs(int[][] grid, int i, int j){
-        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length)
-            return false;
-        if(grid[i][j]==1 || grid[i][j]==2)
-            return true;
-        grid[i][j] = 2;
-        boolean up = dfs(grid, i-1, j);
-        boolean down = dfs(grid, i+1, j);
-        boolean left = dfs(grid, i, j-1);
-        boolean right = dfs(grid, i, j+1);
-        return up&&down&&right&&left;
+        for(int i: map.keySet()){
+            if(nums.length/map.get(i)>(nums.length)/3)
+                res.add(i);
+        }
+        return res;
 
     }
 
