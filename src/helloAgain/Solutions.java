2f1b9254a7007d7925fda82cde6a3dd44f86351f
dell <arjunpathak164@gmail.com>
2023-04-10 13:02:15 +0530

    hyy

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,44 +9,56 @@ import java.util.*;
 
 public class Solutions {
 
-    public static int numEnclaves(int[][] grid) {
-        int ones = 0;
-        for(int i = 0;i< grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                if(i*j==0 || i==grid.length-1 || j==grid[0].length - 1){
-                    if(grid[i][j]==1)
-                        dfs(grid, i, j);
-                }
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int index=  0;
+        while(index<s.length()){
+            char ch = s.charAt(index);
+            if(ch=='{' || ch=='(' || ch=='[')
+                stack.push(ch);
+            else {
+                if(!stack.isEmpty() && ch==')' && stack.peek()=='(')
+                    stack.pop();
+                else if (!stack.isEmpty() && ch=='}' && stack.peek()=='{')
+                    stack.pop();
+                else if(!stack.isEmpty() && ch==']' && stack.peek()=='[')
+                    stack.pop();
+                else return false;
             }
-        }
-        for(int i = 0;i< grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                ones += grid[i][j];
-            }
-        }
-        return ones;
-    }
+            index++;
 
-    public static void dfs(int[][] grid, int i, int j){
-        if(i<0 || i>=grid.length || j<0 || j>= grid[0].length || grid[i][j]==0)
-            return;
-        grid[i][j] = 0;
-        dfs(grid, i, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
-        dfs(grid, i+1, j);
+        }
+        return stack.isEmpty();
 
     }
+    // Below solution is wrong because it doesn't take care of order of the brackets.
+    public static boolean isValidWrong(String s) {
+        int curly = 0;
+        int square = 0;
+        int braces = 0;
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i)=='(')
+                curly++;
+            else if(s.charAt(i)=='{')
+                braces++;
+            else if(s.charAt(i)=='[')
+                square++;
+            else if(s.charAt(i)==')')
+                curly--;
+            else if(s.charAt(i)=='}')
+                braces--;
+            else if(s.charAt(i)==']')
+                square--;
+        }
+        return curly==0 &&braces==0 && square==0;
 
-
-
-
+    }
 
 
     public static void main(String[] args){
-        int[][] nums = {{0,1,1,0},{0,0,1,0},{0,0,1,0},{0,0,0,0}};
+        String s = "(]";
 
-        System.out.println(numEnclaves(nums));
+        System.out.println(isValid(s));
 
     }
 
