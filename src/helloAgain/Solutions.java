332c6949dd570acb897d08b9fc73cab9ce7bf5ec
dell <arjunpathak164@gmail.com>
2023-04-11 15:54:19 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,23 +8,30 @@ import java.util.*;
 
 
 public class Solutions {
-
-    public String removeStars(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = s.length()-1;i>=0;i--){
-            int stars = 0;
-            if(s.charAt(i)!='*' && stars==0) {
-                sb.append(s.charAt(i));
-            }
-            else if(s.charAt(i)=='*') {
-                stars++;
-            }
-            else if(s.charAt(i)!='*' && stars!=0) {
-                    stars--;
+    int[][][] dp;
+    public int numberOfPaths(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        dp = new int[m][n][51];
+        for(int[][] g:dp){
+            for(int[] gf:g){
+                Arrays.fill(gf, -1);
             }
         }
-        sb.reverse();
-        return sb.toString();
+        return helper(0, 0, grid, k, 0);
+
+    }
+
+    public int helper(int i, int j, int[][] grid, int k, int sum){
+        if(i<0 || i>= grid.length || j<0 || j>=grid[0].length)
+            return 0;
+        sum+= grid[i][j];
+        if(i==grid.length-1 && j==grid[0].length)
+            return sum%k==0?1:0;
+        if(dp[i][j][sum%k]!=-1)
+            return dp[i][j][sum%k];
+        dp[i][j][sum%k] = (helper(i+1, j, grid, k, sum) + helper(i, j+1, grid, k, sum))%1000000007;
+        return dp[i][j][sum%k];
 
     }
 
