733dda7399e92f166c65ac50cc9a369f48ac869e
dell <arjunpathak164@gmail.com>
2023-04-13 16:37:41 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,24 +8,21 @@ import java.util.*;
 
 
 public class Solutions {
-    public String simplifyPath(String path) {
-        StringBuilder sb = new StringBuilder();
-        Stack<String> s = new Stack<>();
-        String[] arr = path.split("/");
-
-        for(int i = 0;i<arr.length;i++){
-            if(!s.isEmpty() && arr[i].equals(".."))
-                s.pop();
-            else if(!arr[i].equals("") && !arr[i].equals(".") && !arr[i].equals(".."))
-                s.push(arr[i]);
+    public int maxSubArray(int[] nums) {
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        while(end< nums.length){
+            if(sum<0 && nums[end]>=sum){
+                start = end;
+                sum = 0;
+            }
+            sum += nums[end];
+            max = Math.max(sum, max);
+            end++;
         }
-
-        if(s.isEmpty())
-            return "/";
-        while(!s.isEmpty()){
-            sb.insert(0, s.pop()).insert(0, "/");
-        }
-        return sb.toString();
+        return max;
 
     }
 
