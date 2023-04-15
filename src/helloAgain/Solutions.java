432547bef4e88631c5b0eb89d25a4a21005e74ec
dell <arjunpathak164@gmail.com>
2023-04-15 21:36:44 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,17 +8,38 @@ import java.util.*;
 
 
 public class Solutions {
-    public static long[] findPrefixScore(int[] nums) {
-        long res[] = new long[nums.length];
-        res[0] = nums[0] *2;
-        int max = nums[0];
-        for(int i = 1;i< nums.length;i++){
-            if(nums[i]>max)
-                max = nums[i];
-            res[i] = res[i-1] + (max+nums[i]);
+    public static int[] findColumnWidth(int[][] grid) {
+        int[] res = new int[grid[0].length];
+        int RI = 0;
+        int index = 0;
+        int max = 0;
+        while(index<grid[0].length){
+            for(int i = 0;i< grid.length;i++){
+                if(digits(grid[i][index])>max)
+                    max = digits(grid[i][index]);
+            }
+            res[RI] = max;
+            RI++;
+            max = 0;
+            index++;
         }
         return res;
 
+
+    }
+
+    public static int digits(int n){
+        if(n==0)
+            return 1;
+        int count = 0;
+        int k = Math.abs(n);
+        while(k!=0){
+            k = k/10;
+            count++;
+        }
+        if(n<0)
+            return count+1;
+        return count;
     }
 
 
