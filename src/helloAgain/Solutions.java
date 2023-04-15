83c6563c6adc69eee69fc38210b93f6b4d36523b
dell <arjunpathak164@gmail.com>
2023-04-15 18:58:56 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,43 +8,33 @@ import java.util.*;
 
 
 public class Solutions {
+    Integer[][] dp;
 
-    public int minCost(int[] startPos, int[] homePos, int[] rowCosts, int[] colCosts) {
-        int cost = 0;
-        if(homePos[0]>startPos[0]){
-            int i = startPos[0] + 1;
-            while(i<=homePos[0]){
-                cost += rowCosts[i];
-                i++;
-            }
+    public int maxValueOfCoins(List<List<Integer>> piles, int k) {
+        int n = piles.size();
+        dp = new Integer[n+1][k+1];
+        return helper(piles, n-1, k);
+
+    }
+
+    public int helper(List<List<Integer>> piles, int curr, int left){
+        if(curr==0 || left==0)
+            return 0;
+        if(dp[curr][left]!=null)
+            return dp[curr][left];
+        int limit = Math.max(left, piles.get(curr).size());
+
+        int dontTake = helper(piles, curr - 1, left);
+
+        int Take = 0;
+        for(int j = 0, sum = 0;j<limit;j++){
+            sum += piles.get(curr).get(j);
+            Take = Math.max(sum + helper(piles, curr-1, left - (j+1)), Take);
         }
 
-        if(homePos[0]<startPos[0]){
-            int i =startPos[0] - 1;
-            while(i>=homePos[0]){
-                cost += rowCosts[i];
-                i--;
-            }
-        }
-
-        if(startPos[1]<homePos[1]){
-            int i = startPos[1] + 1;
-            while(i<=homePos[1]){
-                cost += colCosts[i];
-                i++;
-            }
-        }
-
-        if(startPos[1]>homePos[1]){
-            int i = startPos[1] - 1;
-            while(i>=homePos[1]){
-                cost += colCosts[i];
-                i--;
-            }
-        }
-        return cost;
-
-
+        int res = Math.max(Take, dontTake);
+        dp[curr][left] = res;
+        return res;
     }
 
 
