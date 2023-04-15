5c08e666907c88de657e428227711adae0a70d6b
dell <arjunpathak164@gmail.com>
2023-04-15 23:03:04 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,38 +8,29 @@ import java.util.*;
 
 
 public class Solutions {
-    public static int[] findColumnWidth(int[][] grid) {
-        int[] res = new int[grid[0].length];
-        int RI = 0;
-        int index = 0;
-        int max = 0;
-        while(index<grid[0].length){
-            for(int i = 0;i< grid.length;i++){
-                if(digits(grid[i][index])>max)
-                    max = digits(grid[i][index]);
+    public int countSquares(int[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for(int i = 0;i< matrix.length;i++){
+            dp[i][0] = matrix[i][0];
+        }
+        for(int i = 0;i<matrix[0].length;i++){
+            dp[0][i] = matrix[0][i];
+        }
+        for(int i = 1;i< matrix.length;i++){
+            for(int j = 1;j<matrix[0].length;j++){
+                if(matrix[i][j]==1){
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
+                }
             }
-            res[RI] = max;
-            RI++;
-            max = 0;
-            index++;
         }
-        return res;
-
-
-    }
-
-    public static int digits(int n){
-        if(n==0)
-            return 1;
-        int count = 0;
-        int k = Math.abs(n);
-        while(k!=0){
-            k = k/10;
-            count++;
+        int sum = 0;
+        for(int i = 0;i< matrix.length;i++){
+            for(int j = 0;j<matrix[0].length;j++){
+                sum += dp[i][j];
+            }
         }
-        if(n<0)
-            return count+1;
-        return count;
+        return sum;
+
     }
 
 
