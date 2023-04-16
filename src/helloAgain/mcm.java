f39b9865ab9f3ffffc663e3723cfb2efb32cfc02
dell <arjunpathak164@gmail.com>
2023-04-16 11:40:41 +0530

    hyj

--- src/helloAgain/mcm.java
+++ src/helloAgain/mcm.java
@@ -7,52 +7,31 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class mcm {
-    public static int twoEggDrop(int n) {
-        int[][] dp = new int[3][n+1];
-        for(int[] i:dp){
-            Arrays.fill(i, -1);
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int[] res = new int[2];
+        int index = 0;
+        int max = 0;
+        for(int i = 0;i< mat.length;i++){
+            int ones = 0;
+            for(int j = 0;j<mat[0].length;j++){
+                if(mat[i][j]==1){
+                    ones++;
+                }
+            }
+            if(ones>max){
+                max = ones;
+                index = i;
+            }
         }
-        return helper(2, n, dp);
-
-    }
-    public static int helper(int k, int n, int[][] dp){
-        if(n==0 || n==1 || k==1)
-            return n;
-        if(dp[k][n]!=-1)
-            return dp[k][n];
-        int min = Integer.MAX_VALUE;
-        for(int i = 1;i<=n;i++){
-            int temp = 1 + Math.max(helper(k-1, i-1, dp), helper(k, n-i, dp));
-            min = Math.min(min, temp);
-        }
-        return dp[k][n] = min;
+        res[0] = index;
+        res[1] = max;
+        return res;
     }
 
-    public  static int f(int eggs,int floors,int[][] memo) {
-        if(floors == 0 || floors == 1) return floors;
-        if(eggs == 1) return floors;
-
-        int ans = Integer.MAX_VALUE;
-        if(memo[eggs][floors] != -1) return memo[eggs][floors];
-
-        for(int i = 1;i<=floors;i++) {
-            int min = Math.max(f(eggs-1,i-1,memo),f(eggs,floors-i,memo));
-            if(min < ans) ans = min;
-        }
-
-        return memo[eggs][floors] = ans+1;
-    }
-    public static int twoEggDropw(int n) {
-        int floors = n;
-        int eggs = 2;
-        int[][] memo = new int[eggs+1][floors+1];
-        for(int[] arr: memo) Arrays.fill(arr,-1);
-        return f(eggs,floors,memo);
-    }
 
 
     public static void main(String[] args){
-        String s = "aab";
-        System.out.println(twoEggDropw(100));
+        String a = "aaa";
+        //System.out.println(addMinimum(a));
     }
 }
