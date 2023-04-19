0a52ee6ff0ebfc677e9d51a8923d3db4f02fdf5e
dell <arjunpathak164@gmail.com>
2023-04-19 18:09:32 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,19 +8,23 @@ import java.util.*;
 
 
 public class Solutions {
-    public static int maxOperations(int[] nums, int k) {
-        //HashSet<Integer> set = new HashSet<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        for(int i = 0;i< nums.length;i++){
-            if(map.containsKey(k - nums[i]) && map.get(k - nums[i])>0){
-                count++;
-                map.put(k - nums[i], map.get(k - nums[i])-1);
+    public long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+        if(skill.length==2)
+            return skill[0]*skill[1];
 
-            }
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        long res = 0;
+        int k = skill[0]+skill[skill.length-1];
+        int index = 0;
+        while(index< skill.length/2){
+            if(skill[index] + skill[skill.length - index]==k){
+                res += (skill[index]*skill[skill.length - index]);
+                index++;
+
+            }else return -1;
         }
-        return count;
+        return res;
+
 
     }
 
@@ -30,7 +34,7 @@ public class Solutions {
         int[] nums = {1, 2, 3, 4,};
 
 
-        System.out.println(maxOperations(nums, 5));
+        //System.out.println(maxOperations(nums, 5));
 
     }
 
