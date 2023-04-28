61bffe28c8801d50e601e9bd1ee4915fda11811a
dell <arjunpathak164@gmail.com>
2023-04-28 10:03:27 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,44 +8,41 @@ import java.util.*;
 
 
 public class Solutions {
-    public int lastStoneWeightUsingPriorityQueue(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int st:stones){
-            pq.add(st);
-        }
-        while(pq.size()>1){
-            int p = pq.poll();
-            int q = pq.poll();
-            if(p!=q){
-                pq.add(p - q);
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        boolean[] visied = new boolean[n];
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            if(!visied[i]){
+                dfs(strs, visied, i);
+                count++;
             }
         }
-        return pq.isEmpty()? 0:pq.peek();
-    }
-
-    public int lastStoneWeight(int[] stones) {
-        ArrayList<Integer> ar = new ArrayList<>();
-        for(int i=0;i<stones.length;i++){
-            ar.add(stones[i]);
-        }
-
-        while(ar.size()>1){
-            Collections.sort(ar);
-            int y = ar.get(ar.size()-1);
-            ar.remove(new Integer((y)));
-            int x = ar.get(ar.size()-1);
-            ar.remove(new Integer(x));
-            if(x!=y){
-                ar.add(y-x);
-            }
-        }
-
-        if(!ar.isEmpty()){
-            return ar.get(0);
-        }
-        return 0;
+        return count;
 
     }
+
+    public void dfs(String[] strs, boolean[] visited, int i){
+        visited[i] = true;
+        for(int j = 0;j< strs.length;j++){
+            if(!visited[j] && isSimilar(strs[i],strs[j] ))
+                dfs(strs, visited, j);
+
+        }
+    }
+
+    public boolean isSimilar(String s1, String s2){
+        int diff = 0;
+        for(int i = 0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i))
+                diff++;
+            if(diff>2)
+                return false;
+        }
+        return true;
+    }
+
+
 
 
 
