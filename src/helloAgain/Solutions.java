a5ccae4b9f2f5db90ba78935d04bea7bd44807a4
dell <arjunpathak164@gmail.com>
2023-05-09 21:38:18 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,24 +8,52 @@ import java.util.*;
 
 
 public class Solutions {
-    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        Arrays.sort(arr);
-        arr[0] = 1;
-        for(int i = 1;i<arr.length;i++){
-            if(Math.abs(arr[i-1] - arr[i])<=1)
-                continue;
-            else arr[i] = arr[i-1]+1;
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res=  new ArrayList<>();
+        if(matrix.length==0 || matrix[0].length==0)
+            return res;
+        int top = 0;
+        int left = 0;
+        int right = matrix.length;
+        int bottom = matrix[0].length;
+
+        while(true){
+            for(int i = left;i<=right;i++){
+                res.add(matrix[top][i]);
+                top++;
+
+                if(left>right || top>bottom)
+                    break;
+            }
+
+            for(int i = top;i<=bottom;i++){
+                res.add(matrix[i][right]);
+                right--;
+
+                if(left>right || top>bottom)
+                    break;
+            }
+
+            for(int i = right;i>=left;i--){
+                bottom--;
+                res.add(matrix[bottom][i]);
+                if(left>right || top>bottom)
+                    break;
+            }
+
+            for(int i = bottom;i>=top;i--){
+                left++;
+                res.add(matrix[i][left]);
+                if(left>right || top>bottom)
+                    break;
+            }
+            return res;
         }
-        return arr[arr.length-1];
 
 
     }
-
     public static void main(String[] args){
         int[] nums = {1, 2, 3, 4,};
-
-
-        //System.out.println(maxOperations(nums, 5));
 
     }
 
