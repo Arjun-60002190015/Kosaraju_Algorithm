869f9529287c8d956a2a10f9922c3ea12c24db75
dell <arjunpathak164@gmail.com>
2023-05-10 20:04:16 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,33 +8,19 @@ import java.util.*;
 
 
 public class Solutions {
-    public int[][] generateMatrix(int n) {
-        int[][] res = new int[n][n];
-        int left = 0;
-        int top =0;
-        int right = n-1;
-        int bottom = n-1;
-        int count=  0;
-        while(left<=right){
-            for(int j = left;j<=right;j++){
-                res[top][j] = count++;
+    public int findLongestChain(int[][] pairs) {
+        int count = 1;
+        int prev = pairs[0][1];
+        Arrays.sort(pairs, (a, b) -> {return a[0] - b[0];});
+        for(int i = 1;i< pairs.length;i++){
+            if(prev<pairs[i][0]) {
+                prev = pairs[i][1];
+                count++;
+            }else{
+                prev = Math.min(prev, pairs[i][1]);
             }
-            top++;
-            for(int i = top;i<=bottom;i++){
-                res[i][right] = count++;
-            }
-            right--;
-            for(int j = right;j>=left;j--){
-                res[bottom][j] = count++;
-            }
-            bottom--;
-
-            for(int i = bottom;i>=top;i--){
-                res[i][left] = count++;
-            }
-            left++;
         }
-        return res;
+        return count;
 
     }
 
