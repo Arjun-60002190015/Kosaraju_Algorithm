c9d264d5f2722ca1efa0afe3912dc14c57be5e84
dell <arjunpathak164@gmail.com>
2023-05-11 12:44:30 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,22 +8,34 @@ import java.util.*;
 
 
 public class Solutions {
-    public int trap(int[] height) {
-        int left[] = new int[height.length];
-        int right[] = new int[height.length];
-        left[0] = height[0];
-        for(int i = 1;i< height.length;i++){
-            left[i] = Math.max(left[i-1], height[i]);
+    public int maxAreaBruteForce(int[] height) {
+        int n = height.length;
+        int max = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                int area = Math.min(height[i],height[j]) * (j-i);
+                max = Math.max(max, area);
+            }
         }
-        right[height.length-1] = height[height.length-1];
-        for(int i = height.length-2;i>=0;i--){
-            right[i] = Math.max(right[i+1], height[i]);
+        return max;
+    }
+
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int left = 0;
+        int right = n-1;
+        int max = Math.min(height[left], height[right]) * (right - left);
+        while(left<right){
+            if(height[left]<=height[right])
+                left++;
+            else{
+                right--;
+            }
+
+            max = Math.max(max, Math.min(height[left], height[right])*(right-left));
+
         }
-        int sum = 0;
-        for(int i = 0;i< height.length;i++){
-            sum += Math.min(left[i], right[i]) - height[i];
-        }
-        return sum;
+        return max;
 
     }
 
