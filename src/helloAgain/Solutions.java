f9affa0be7cf2c49d9c5783799cdb7958b46c46e
dell <arjunpathak164@gmail.com>
2023-05-18 19:59:12 +0530

    hyj

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,47 +8,23 @@ import java.util.*;
 
 
 public class Solutions {
-    //bruteForce
-    public int[] maxSlidingWindowBruteForce(int[] nums, int k) {
-        if(nums==null || k<=0)
-            return new int[0];
-        int[] arr = new int[nums.length-k+1];
-        for(int i = 0;i< nums.length-k+1;i++){
-            int max = Integer.MIN_VALUE;
-            for(int j = i;j<i+k;j++){
-                max = Math.max(nums[j], max);
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length-1;
+        while(i< matrix.length && j>=0){
+            if(matrix[i][j]==target)
+                return true;
+            if(matrix[i][j]<target){
+                i++;
             }
-            arr[i] = max;
+            else {
+                j--;
+            }
+
         }
-        return arr;
-
-    }
-
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        if(nums==null || k<=0)
-            return new int[0];
-
-        int index = 0;
-        int[] r = new int[n-k+1];
-        Deque<Integer> pq = new ArrayDeque<>();
-        for(int i = 0;i< nums.length;i++){
-            while(!pq.isEmpty() && pq.peek()<i-k+1){
-                pq.poll();
-            }
-
-            while(!pq.isEmpty() && nums[pq.peek()] < nums[i]){
-                pq.pollLast();
-            }
-
-            pq.offer(i);
-            if(i>=k-1){
-                r[index] = nums[pq.peek()];
-                index++;
-            }
-        }
-        return r;
-
+        return false;
 
     }
 
