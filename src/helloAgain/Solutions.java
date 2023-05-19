c261318b6e7552b4ff24cd9d3b495b2748c8907c
dell <arjunpathak164@gmail.com>
2023-05-19 20:18:08 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,21 +9,26 @@ import java.util.*;
 
 public class Solutions {
 
-    public int robUsingRecursion(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return helper(nums,n-1, dp);
+    public int rob(int[] nums) {
+        int[] dp1 = new int[nums.length];
+        Arrays.fill(dp1, -1);
+        dp1[0] = nums[1];
+
+        int[] dp2 = new int[nums.length];
+        Arrays.fill(dp2, -1);
+        dp2[0] = nums[0];
+        return Math.max(helper(nums, 1, nums.length-1, dp1), helper(nums, 0, nums.length-2, dp2));
+
     }
 
-    public int helper(int[] nums, int i, int[] dp){
-        if(i==0)
-            return nums[0];
-        if(i==1)
-            return Math.max(nums[0], nums[1]);
+    public int helper(int[] nums, int start, int i, int[] dp){
+        if(i<start)
+            return 0;
+        if(i==start)
+            return nums[start];
         if(dp[i]!=-1)
             return dp[i];
-        return dp[i] = Math.max(helper(nums, i-1, dp), nums[i]+ helper(nums, i-2, dp));
+        return dp[i] = Math.max(helper(nums, start, i-1, dp), nums[i] + helper(nums, start, i-2, dp));
     }
 
 
