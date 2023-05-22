e45b3db8c463f580e314e9e96bd48257e22737fe
dell <arjunpathak164@gmail.com>
2023-05-22 11:02:27 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,26 +9,21 @@ import java.util.*;
 
 public class Solutions {
 
-    public int rob(int[] nums) {
-        int[] dp1 = new int[nums.length];
-        Arrays.fill(dp1, -1);
-        dp1[0] = nums[1];
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i:nums){
+            map.put(i, map.getOrDefault(i, 0)+1 );
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> map.get(a) - map.get(b));
+        pq.addAll(map.keySet());
 
-        int[] dp2 = new int[nums.length];
-        Arrays.fill(dp2, -1);
-        dp2[0] = nums[0];
-        return Math.max(helper(nums, 1, nums.length-1, dp1), helper(nums, 0, nums.length-2, dp2));
+        int[] res = new int[k];
+        for(int i = 0;i<k;i++){
+            res[i] = pq.poll();
+        }
+        return res;
 
-    }
 
-    public int helper(int[] nums, int start, int i, int[] dp){
-        if(i<start)
-            return 0;
-        if(i==start)
-            return nums[start];
-        if(dp[i]!=-1)
-            return dp[i];
-        return dp[i] = Math.max(helper(nums, start, i-1, dp), nums[i] + helper(nums, start, i-2, dp));
     }
 
 
