64adb38d125bef55111f2890f5068ff17b1bc014
dell <arjunpathak164@gmail.com>
2023-05-22 19:40:03 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,80 +8,21 @@ import java.util.*;
 
 
 public class Solutions {
-    boolean visited[][];
-    int n;
+    public int minLength(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        st.push(s.charAt(0));
+        for(int i = 1;i<n;i++){
+            if(!st.isEmpty() && ((s.charAt(i)=='B' && st.peek()=='A') ||
+                    (s.charAt(i)=='D' && st.peek()=='C'))){
+                st.pop();
 
-    public int shortestBridge(int[][] grid) {
-        n = grid.length;
-        visited = new boolean[n][n];
-
-        Queue<int[]> queue = new ArrayDeque<>();
-        boolean flag = false;
-
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                if(grid[i][j]==1) {
-                    dfs(i, j, grid, queue);
-                    flag = true;
-                    break;
-                }
+            }else{
+                st.push(s.charAt(i));
             }
-            if(flag)
-                break;
         }
-        return bfs(grid, queue);
-    }
+        return st.size();
 
-    public void dfs(int i, int j, int[][] grid, Queue<int[]> queue){
-        if(i==-1 || j==-1 || i>=n || j>=n || visited[i][j])
-            return;
-
-        visited[i][j] = true;
-        if(grid[i][j]==0) {
-            queue.add(new int[]{i, j});
-            return;
-        }
-
-        dfs(i-1, j, grid, queue);
-        dfs(i+1, j, grid, queue);
-        dfs(i, j-1, grid, queue);
-        dfs(i, j+1, grid, queue);
-    }
-
-    public int bfs(int[][] grid, Queue<int[]> queue){
-        int level = 0;
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            while(size-->0){
-                int[] curr = queue.remove();
-                int i = curr[0];
-                int j = curr[1];
-                if(grid[i][j] == 1)
-                    return level;
-
-                if(i+1<n && !visited[i+1][j]){
-                    queue.add(new int[]{i+1, j});
-                    visited[i+1][j] = true;
-                }
-
-                if(i-1>=0 && !visited[i-1][j]){
-                    queue.add(new int[]{i-1, j});
-                    visited[i-1][j] = true;
-                }
-
-                if(j+1<n && !visited[i][j+1]){
-                    queue.add(new int[]{i, j+1});
-                    visited[i][j+1] = true;
-                }
-
-                if(j-1>=0 && !visited[i][j-1]){
-                    queue.add(new int[]{i, j-1});
-                    visited[i][j-1] = true;
-                }
-            }
-            level++;
-        }
-        return level;
     }
 
 
