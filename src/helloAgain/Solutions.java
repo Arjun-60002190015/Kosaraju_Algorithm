bf267bf948d4c162b2a534bc5bfcab2d2663e797
dell <arjunpathak164@gmail.com>
2023-05-25 19:16:34 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,30 +8,25 @@ import java.util.*;
 
 
 public class Solutions {
-    public long maxScore(int[] nums1, int[] nums2, int k) {
-        int n = nums1.length;
-        int[][] temp = new int[n][2];
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        int[][] temp = new int[n][n];
         for(int i = 0;i<n;i++){
-            temp[i] = new int[]{nums1[i], nums2[i]};
+            temp[i] = new int[]{efficiency[i], speed[i]};
         }
-        Arrays.sort(temp, (a,b)-> b[1] - a[1]);
+        Arrays.sort(temp, (a, b)-> b[0] - a[0]);
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k, (a, b)-> a-b);
+        long res = 0, sum = 0;
+        for(int[] tem:temp){
+            queue.add(tem[1]);
+            sum += tem[1];
+            if(queue.size()>k){
+                sum -= queue.poll();
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        long sum = 0;
-        for(int i = 0;i<k;i++){
-            sum += temp[i][0];
-            queue.add(temp[i][0]);
+            }
+            res = Math.max(res, (sum * tem[0]));
         }
+        return (int)(res%(1e9+7));
 
-        long ans = sum * temp[k-1][1];
-
-        for(int i = k;i<n;i++){
-            sum += temp[i][0] - queue.poll();
-            queue.add(temp[i][0]);
-
-            ans = Math.max(sum * temp[i][1], ans);
-        }
-        return ans;
 
     }
 
