161ad2e8ae59354741892d85b7976e499f58c076
dell <arjunpathak164@gmail.com>
2023-05-25 12:02:07 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,20 +8,30 @@ import java.util.*;
 
 
 public class Solutions {
-    public int minLength(String s) {
-        Stack<Character> st = new Stack<>();
-        int n = s.length();
-        st.push(s.charAt(0));
-        for(int i = 1;i<n;i++){
-            if(!st.isEmpty() && ((s.charAt(i)=='B' && st.peek()=='A') ||
-                    (s.charAt(i)=='D' && st.peek()=='C'))){
-                st.pop();
-
-            }else{
-                st.push(s.charAt(i));
-            }
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        int[][] temp = new int[n][2];
+        for(int i = 0;i<n;i++){
+            temp[i] = new int[]{nums1[i], nums2[i]};
         }
-        return st.size();
+        Arrays.sort(temp, (a,b)-> b[1] - a[1]);
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        long sum = 0;
+        for(int i = 0;i<k;i++){
+            sum += temp[i][0];
+            queue.add(temp[i][0]);
+        }
+
+        long ans = sum * temp[k-1][1];
+
+        for(int i = k;i<n;i++){
+            sum += temp[i][0] - queue.poll();
+            queue.add(temp[i][0]);
+
+            ans = Math.max(sum * temp[i][1], ans);
+        }
+        return ans;
 
     }
 
