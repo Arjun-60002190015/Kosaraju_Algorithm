0c10a85d66a2eda78d78f5e052ad96c3980f1327
dell <arjunpathak164@gmail.com>
2023-05-26 19:52:04 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,26 +8,27 @@ import java.util.*;
 
 
 public class Solutions {
-    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int[][] temp = new int[n][n];
-        for(int i = 0;i<n;i++){
-            temp[i] = new int[]{efficiency[i], speed[i]};
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        if(n==1)
+            return true;
+        int sum = 0;
+        for(int i:nums){
+            sum += i;
         }
-        Arrays.sort(temp, (a, b)-> b[0] - a[0]);
-        PriorityQueue<Integer> queue = new PriorityQueue<>(k, (a, b)-> a-b);
-        long res = 0, sum = 0;
-        for(int[] tem:temp){
-            queue.add(tem[1]);
-            sum += tem[1];
-            if(queue.size()>k){
-                sum -= queue.poll();
+        int ans = predict(nums, 0, n-1);
+        if(ans<(sum - ans))
+            return false;
+        return true;
 
-            }
-            res = Math.max(res, (sum * tem[0]));
-        }
-        return (int)(res%(1e9+7));
+    }
 
-
+    public int predict(int[] nums, int start, int end){
+        if(start>end)
+            return 0;
+        int one = nums[start] + Math.min(predict(nums, start+2, end), predict(nums, start+1, end-1));
+        int two = nums[end] + Math.min(predict(nums, start+1, end-1), predict(nums, start, end-2));
+        return Math.max(one, two);
     }
 
 
