025d2995133dcd1cced470121acb8a7e53841430
dell <arjunpathak164@gmail.com>
2023-05-27 19:57:11 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,62 +8,18 @@ import java.util.*;
 
 
 public class Solutions {
-    //Recursive Solution
-    public String stoneGameIIIRecursive(int[] stoneValue) {
-        int n = stoneValue.length;
-        int diff = helperR(stoneValue, n, 0);
-        if(diff>0) {
-            return "Alice";
+    public List<String> stringMatching(String[] words) {
+        Set<String> set = new HashSet<>();
+        for(int i = 0;i< words.length;i++){
+            String s = words[i];
+            for(int j = 0;j< words.length;j++){
+                if(i!=j && words[j].contains(s)){
+                    set.add(s);
+                }
+            }
         }
-        else if(diff<0){
-            return "Bob";
-        }
-        return "Tie";
+        return new ArrayList<>(set);
 
-    }
-
-    public int helperR(int[] nums, int n, int i){
-        if(i==n)
-            return 0;
-        int res = nums[i] - helperR(nums, n, i+1);
-        if(i+2 <= n){
-            res = Math.max(res, nums[i]+nums[i+1] - helperR(nums, n, i+2));
-        }
-        if(i+3 <=n){
-            res = Math.max(res, nums[i] + nums[i+1] + nums[i+2] - helperR(nums, n, i+3));
-        }
-        return res;
-    }
-
-    //Dp Solution
-
-    public String stoneGameIII(int[] stoneValue) {
-        int n = stoneValue.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        int diff = helper(stoneValue, dp, n, 0);
-        if(diff>0){
-            return "Alice";
-        }else if(diff<0)
-            return "Bob";
-        return "Tie";
-
-    }
-
-    public int helper(int[] nums, int[] dp, int n, int i){
-        if(i==n)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int res = nums[i] - helper(nums, dp, n, i+1);
-        if(i+1<n){
-            res = Math.max(res, nums[i]+nums[i+1] - helper(nums, dp, n, i+2));
-
-        }
-        if(i+2<n){
-            res = Math.max(res, nums[i]+nums[i+1]+nums[i+2] - helper(nums, dp, n, i+3));
-        }
-        return dp[i] = res;
     }
 
 
