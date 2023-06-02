dc808645b3840758321632e22d89d67f7782e84c
dell <arjunpathak164@gmail.com>
2023-06-02 12:37:58 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,48 +8,36 @@ import java.util.*;
 
 
 public class Solutions {
-
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid==null || grid.length==0 || grid[0].length==0)
-            return -1;
-
+    int count = 0;
+    public int maximumDetonation(int[][] bombs) {
+        int n = bombs.length;
         int ans = 0;
-        int row = grid.length;
-        int col = grid[0].length;
-        if(grid[0][0]==1 || grid[row-1][col-1]==1)
-            return -1;
+        for(int i = 0;i<n;i++){
+            count = 0;
+            dfs(i, new boolean[n], bombs);
+            ans = Math.max(ans, count);
+        }
+        return ans;
 
-        int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
-                {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-        boolean[][] visited = new boolean[row][col];
+    }
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0});
-        visited[0][0] = true;
-        int len = 0;
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            len++;
-            for(int i = 0;i<len;i++){
-                int[] curr = queue.poll();
-                if(curr[0]==row-1 && curr[1]==col-1)
-                    return len;
-
-                for(int[] dir:directions){
-                    int x = curr[0]+dir[0];
-                    int y = curr[1] + dir[1];
-
-                    if(x<0 || x>=row || y<0 || y>=col || visited[x][y]
-                    || grid[x][y]==1)
-                        continue;
-
-                    visited[x][y] = true;
-                    queue.offer(new int[]{x, y});
-                }
+    public void dfs(int index, boolean[] v, int[][] bombs){
+        count++;
+        v[index] = true;
+        int n = bombs.length;
+        for(int i = 0;i<n;i++){
+            if(!v[i] && inRange(bombs[index], bombs[i])){
+                v[i] = true;
+                dfs(i, v, bombs);
             }
         }
-        return -1;
+    }
 
+    public boolean inRange(int[] a, int[] b){
+        long x = a[0] - b[0];
+        long y = a[1] - b[1];
+        long r = a[2];
+        return x*x + y*y <= r*r;
     }
 
     public static void main(String[] args){
