b9e2822973d6df7707f3a9f23e14bd1d3e5abeba
dell <arjunpathak164@gmail.com>
2023-06-03 15:49:24 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,36 +8,28 @@ import java.util.*;
 
 
 public class Solutions {
-    public int orangesRotting(int[][] grid) {
-        int l = 2;
-        for(int i = 0;i< grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                if(grid[i][j]==2){
-                    dfs(i, j, 2, grid);
-                }
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i = 0;i<n;i++){
+            if(manager[i]!=-1){
+                list.get(manager[i]).add(i);
             }
         }
-        for(int i = 0;i<grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                if(grid[i][j]==1) {
-                    return -1;
-                }
-                l = Math.max(l, grid[i][j]);
-            }
-        }
-        return l-2;
+        return helper(headID, list, informTime);
 
     }
 
-    public void dfs(int i, int j, int level, int[][] grid){
-        if(i<0 || i>= grid.length || j<0 || j>=grid[0].length || grid[i][j]==0
-         || grid[i][j]>1 && grid[i][j]<level)
-            return;
-        grid[i][j] = level;
-        dfs(i+1, j, level+1, grid);
-        dfs(i-1, j, level+1, grid);
-        dfs(i, j+1, level+1, grid);
-        dfs(i, j-1, level+1, grid);
+    public int helper(int i, List<List<Integer>> list, int[] time){
+        int max = 0;
+        for(int emp:list.get(i)){
+            int timex = helper(emp, list, time);
+            max = Math.max(max, timex);
+        }
+
+        return max + time [i];
     }
 
     public static void main(String[] args){
