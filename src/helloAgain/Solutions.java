fbbcc1e93e250a7051cde358e2f2547b0ba74480
dell <arjunpathak164@gmail.com>
2023-06-04 20:50:58 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,38 +8,11 @@ import java.util.*;
 
 
 public class Solutions {
-    public int[] findDiagonalOrder(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int[] res = new int[m*n];
-        int index = 0;
-        int i, j = 0;
-        res[index++] = mat[i][j];
-        boolean isUp = true;
-        while(index!= res.length){
-            if(isUp){
-                j += 1;
-                while(j!=0 && i!= m-1){
-                    if(j<n){
-                        res[index++] = mat[i][j];
-                    }
-                    i++;
-                    j--;
-                }
-                res[index++] = mat[i][j];
-                isUp = false;
-            }
-            else{
-                i+= 1;
-                while(i!=0 && j!=n-1){
-                    if(i<m){
-                        res[index++] = mat[i][j];
-                    }
-                    i--;
-                    j++;
-                }
-                res[index++] = mat[i][j];
-                isUp = true;
+    public int[][] transpose(int[][] matrix) {
+        int[][] res = new int[matrix[0].length][matrix.length];
+        for(int i = 0;i< matrix.length;i++){
+            for(int j = 0;j< matrix[i].length;j++){
+                res[i][j] = matrix[j][i];
             }
         }
         return res;
