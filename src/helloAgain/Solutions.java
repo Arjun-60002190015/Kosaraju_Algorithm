5917e14c6b5969afc0ee8eff998e2422c4dca7f6
dell <arjunpathak164@gmail.com>
2023-06-12 21:18:46 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,59 +9,32 @@ import java.util.*;
 
 public class Solutions {
 
-    //Knapsack variant type - Recursion
-    int max= 0 ;
-    public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        int n = heights.length;
-        if(n==0)
-            return 0;
-        helper(heights, bricks, ladders, 0, n, 0);
-        return max;
+    public long matrixSumQueries(int n, int[][] queries) {
+        boolean[] col = new boolean[n];
+        boolean[] row = new boolean[n];
+        int rowC = 0;
+        int colC = 0;
+        long sum = 0;
+        for(int i = queries.length-1;i>=0;i--){
+            int val = queries[i][2];
+            int type = queries[i][0];
+            int index = queries[i][1];
 
-    }
+            if(type==0 && !row[index]){
+                rowC++;
+                row[index] = true;
+                sum += (n-colC)*val;
+            }
 
-    public void helper(int[] heights, int bricks, int ladders, int index, int n, int count){
-        if(index==n-1) {
-            count = count;
-            max = Math.max(max, count);
-            return;
-        }
-
-        if(heights[index]>=heights[index+1]){
-            helper(heights, bricks, ladders, index+1, n, count+1);
-        }else{
-            int diff = heights[index+1] - heights[index];
-            if(bricks>=diff){
-                helper(heights, bricks - diff, ladders, index+1, n, count+1);
-                if(ladders>0){
-                    helper(heights, bricks, ladders-1, index+1, n, count+1);
-                }
-            }else if(ladders>0){
-                helper(heights, bricks, ladders-1, index+1, n, count+1);
+            if(type==1 && !col[index]){
+                colC++;
+                col[index] = true;
+                sum += (n-rowC)*val;
             }
         }
-        max = Math.max(count, max);
+        return sum;
 
-    }
 
-    // Max-Heap(optimized)
-
-    public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i = 0;i<heights.length-1;i++){
-            if(heights[i]>=heights[i+1])
-                continue;
-            bricks -= heights[i+1] - heights[i];
-            pq.add(heights[i+1] - heights[i]);
-
-            if(bricks<0){
-                bricks += pq.poll();
-                if(ladders>0){
-                    ladders--;
-                }else return i;
-            }
-        }
-        return heights.length-1;
 
     }
 
