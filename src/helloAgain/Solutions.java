70b8a3a02bc88181595779fdda6e620ef3ea1a55
dell <arjunpathak164@gmail.com>
2023-06-18 00:10:00 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -2,68 +2,29 @@ package helloAgain;
 
 import javafx.scene.layout.Priority;
 
+import javax.swing.tree.AbstractLayoutCache;
 import java.sql.Array;
 import java.util.*;
 
 
 
 public class Solutions {
-    //Brute force
-
-    public String longestNiceSubstringBruteForce(String s) {
-        String res = "";
-        for(int i = 0;i<s.length();i++){
-            for(int j = 0;j<s.length();j++){
-                String sub = s.substring(i, j+1);
-                if(isNice(sub)){
-                    if(sub.length()>res.length()){
-                        res = sub;
-                    }
+        public double findMaxAverage(int[] nums, int k) {
+            double res = Integer.MIN_VALUE;
+            int start = 0;
+            int end = 0;
+            int sum = 0;
+            while(end< nums.length){
+                sum += nums[end++];
+                if(end>=k) {
+                    res = Math.max(res, sum / k);
+                    sum -= nums[start++];
                 }
+
             }
-        }
-        return res;
-
-
-    }
-
-    public boolean isNice(String sub){
-        HashSet<Character> set = new HashSet<>();
-        for(char ch:sub.toCharArray()){
-            set.add(ch);
-        }
-
-        for(int i = 0;i<sub.length();i++){
-            char ch = sub.charAt(i);
-            if(!set.contains(Character.toLowerCase(ch))){
-                return false;
-            }
-            else if(!set.contains(Character.toUpperCase(ch))){
-                return false;
-            }
+            return res;
 
         }
-        return true;
-    }
-
-    public String longestNiceSubstringOptimized(String s) {
-        HashSet<Character> set = new HashSet<>();
-        for(char ch:s.toCharArray()){
-            set.add(ch);
-        }
-        for(int i = 0;i<=s.length()-1;i++){
-            char ch = s.charAt(i);
-            if(set.contains(Character.toUpperCase(ch)) && set.contains(Character.toLowerCase(ch)))
-                continue;
-            String one = longestNiceSubstringOptimized(s.substring(0, i));
-            String two = longestNiceSubstringOptimized(s.substring(i+1));
-            return one.length()>=two.length()? one:two;
-        }
-        return s;
-
-
-    }
-
 
     public static void main(String[] args){
         int[] nums = {0, -1};
