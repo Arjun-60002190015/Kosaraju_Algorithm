4dd12c856b5aab34dda14cc18ddce6e19ffb5984
dell <arjunpathak164@gmail.com>
2023-06-18 12:25:32 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,22 +9,25 @@ import java.util.*;
 
 
 public class Solutions {
-        public double findMaxAverage(int[] nums, int k) {
-            double res = Integer.MIN_VALUE;
-            int start = 0;
-            int end = 0;
-            int sum = 0;
-            while(end< nums.length){
-                sum += nums[end++];
-                if(end>=k) {
-                    res = Math.max(res, sum / k);
-                    sum -= nums[start++];
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int count = 0;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        while(end<arr.length){
+            sum += arr[end];
+            if(end-start+1==k){
+                if(sum/k>=threshold){
+                    count++;
                 }
-
+                sum -= arr[start];
+                start++;
             }
-            return res;
-
+            end++;
         }
+        return count;
+
+    }
 
     public static void main(String[] args){
         int[] nums = {0, -1};
