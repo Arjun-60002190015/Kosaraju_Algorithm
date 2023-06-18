e328e9c4a5edb3a7ef219782cfbe2afc3878c328
dell <arjunpathak164@gmail.com>
2023-06-18 22:42:36 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,22 +9,22 @@ import java.util.*;
 
 
 public class Solutions {
-    public int longestSubarray(int[] nums) {
-        int k = 1;
-        int xero = 0;
-        int start = 0;
-        int end = 0;
-        int max = 0;
-        while(end< nums.length){
-            if(nums[end]==0)
-                xero++;
-            if(xero>k){
-                if(nums[start]==0)
-                    xero--;
-                start++;
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int max = k;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0;i<k;i++){
+            map.put(answerKey.charAt(i), map.getOrDefault(answerKey.charAt(i), 0)+1);
+        }
+        int left = 0;
+        int right = k;
+        while(right<answerKey.length()){
+            map.put(answerKey.charAt(right), map.getOrDefault(answerKey.charAt(right), 0)+1);
+            while(Math.min(map.getOrDefault('T', 0), map.getOrDefault('F', 0))>k){
+                map.put(answerKey.charAt(left), map.get(answerKey.charAt(left))-1);
+                left++;
             }
-            max = Math.max(max, end-start);
-            end++;
+            max = Math.max(max, right-left+1);
+            right++;
         }
         return max;
 
