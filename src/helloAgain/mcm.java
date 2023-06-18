06385a1203e0dc7cf7a90089200f8ca0e69e43b6
dell <arjunpathak164@gmail.com>
2023-06-18 21:00:54 +0530

    kggfhfhyt

--- src/helloAgain/mcm.java
+++ src/helloAgain/mcm.java
@@ -7,23 +7,36 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class mcm {
-    public char nextGreatestLetter(char[] letters, char target) {
-        int start = 0;
-        int end = letters.length-1;
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(letters[mid]<=target){
-                start = mid+1;
-            }else end = mid-1;
+    int mod = (int)(1e9+7);
+    public int countPaths(int[][] grid) {
+        int m = grid.length;
+        int n=  grid[0].length;
+        int[][] dp = new int[m+1][n+1];
+        for(int[] i:dp){
+            Arrays.fill(i, -1);
         }
-        return start==letters.length? letters[0]:letters[start];
+        int path = 0;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                path  = (path + helper(grid, i, j, m, n, dp, -1))%mod;
+            }
+        }
+        return path;
 
     }
 
+    public int helper(int[][] grid, int i, int j, int m, int n, int[][] dp, int prev){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]<=prev)
+            return 0;
 
+        if(dp[i][j]!=-1)
+            return dp[i][j];
 
-    public static void main(String[] args){
-        String a = "aaa";
-        //System.out.println(addMinimum(a));
+        int down = helper(grid, i+1, j, m, n, dp, grid[i][j]);
+        int up = helper(grid, i-1, j, m, n, dp, grid[i][j]);
+        int left = helper(grid, i, j+1, m, n, dp, grid[i][j]);
+        int right = helper(grid, i, j-1, m, n, dp, grid[i][j]);
+        return dp[i][j] = (1+ up+down+left+right)%mod;
     }
-}
+
+    }
