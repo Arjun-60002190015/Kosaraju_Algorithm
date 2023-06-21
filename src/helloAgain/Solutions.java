511910b87e6449fb79d91d2496dd559b892358fa
dell <arjunpathak164@gmail.com>
2023-06-21 20:24:09 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,35 +9,20 @@ import java.util.*;
 
 
 public class Solutions {
-    public long minCost(int[] nums, int[] cost) {
-        int left = nums[0];
-        int right = nums[0];
-        for(int i:nums){
-            left = Math.min(left, i);
-            right = Math.max(right, i);
-        }
-        long ans = 0;
-        while(left<right){
-            int mid = (left+right)/2;
-            long one = helper(nums, cost, mid);
-            long two = helper(nums, cost, mid+1);
-            if(one>two){
-                left = mid+1;
-                ans = two;
-            }else{
-                right = mid;
-                ans = one;
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int total = 0;
+        int min = 0;
+        for(int i = 0, curr = 0;i<n;i++){
+            total += cardPoints[i];
+            curr += cardPoints[i];
+            if(i<n-k)min += cardPoints[i];
+            else{
+                curr -= cardPoints[i-(n-k)];
+                min = Math.min(min, curr);
             }
         }
-        return ans;
-    }
-
-    public long helper(int[] nums, int[] cost, int mid){
-        long totalCost = 0L;
-        for(int i = 0;i<nums.length;i++){
-            totalCost += 1L * Math.abs(nums[i]-mid)*cost[i];
-        }
-        return totalCost;
+        return total-min;
     }
 
     public static void main(String[] args){
