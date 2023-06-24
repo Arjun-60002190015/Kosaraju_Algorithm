b2657bb0a703489995f3032a4a2ef3480f3ff711
dell <arjunpathak164@gmail.com>
2023-06-24 15:24:27 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,35 +9,47 @@ import java.util.*;
 
 
 public class Solutions {
-    public int longestBeautifulSubstring(String word) {
-        HashSet<Character> set = new HashSet<>();
-        if(word.length()<5)
+    //Recursion -
+    public int tallestBillboardRecursion(int[] rods) {
+        return helpero(rods, 0, 0, 0);
+
+    }
+
+    public int helpero(int[] rods, int one, int two, int index){
+        if(index== rods.length){
+            if(one==two)
+                return one;
             return 0;
-
-        int end = 1;
-        int curr = 1;
-        int max = 0;
-        set.add(word.charAt(0));
-        while(end<word.length()){
-            if(word.charAt(end)>=word.charAt(end-1)){
-                set.add(word.charAt(end));
-                curr++;
-            }else {
-                if(set.size()==5)
-                    max = Math.max(curr, max);
-                set.clear();
-                curr = 1;
-                set.add(word.charAt(end));
-
-
-            }
-            end++;
-
-
         }
-        if(set.size()==5)
-            max = Math.max(curr, max);
-        return max;
+        int not = helper(rods, one, two, index+1);
+        int pick = helper(rods, one+rods[index], two, index+1);
+        int pi = helper(rods, one, two+rods[index], index+1);
+        return Math.max(not, Math.max(pick,pi));
+    }
+
+    //Memoization
+
+    public int tallestBillboard(int[] rods) {
+        Integer[][] dp = new Integer[rods.length+1][10001];
+        int res = helper(rods, 0, 0, dp);
+        return res<0?0:res;
+
+    }
+
+    public int helper(int[] rods, int index, int sum, Integer[][] dp){
+        if(index==rods.length){
+            if(sum==0){
+                return 0;
+            }
+            return Integer.MIN_VALUE;
+        }
+        if(dp[index][sum+5000]!=null){
+            return dp[index][sum+5000];
+        }
+        int not = helper(rods, index+1, sum, dp);
+        int pick = helper(rods, index+1, sum + rods[index], dp)+ rods[index];
+        int pi = helper(rods, index+1, sum - rods[index], dp);
+        return dp[index][sum+5000] = Math.max(not, Math.max(pick, pi));
 
     }
 
