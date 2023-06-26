1c4861ca581fde444b4a7de12e86ca8b6c4410c0
dell <arjunpathak164@gmail.com>
2023-06-26 15:10:26 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -11,41 +11,23 @@ import java.util.*;
 
 
 public class Solutions {
-    public long totalCost(int[] costs, int k, int candidates) {
-        int n = costs.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> {
-            if(a[0]==b[0]){
-                return a[1] - b[1];
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int prod = 1;
+        int end = 0;
+        int start = 0;
+        int max = 0;
+        while(end<nums.length){
+            prod *= nums[end];
+            while(prod>=k && start<end){
+                prod /= nums[start];
+                start++;
             }
-            return a[0] - b[0];
-        });
-
-        for(int i = 0;i<candidates;i++){
-            pq.offer(new int[]{costs[i], 0});
-        }
-        for(int i = Math.max(candidates, n - candidates);i<costs.length;i++){
-            pq.offer(new int[]{costs[i], 1});
-        }
-        long asnwer = 0;
-        int head = candidates;
-        int tail = n-1-candidates;
-        for(int i = -0;i<k;i++){
-            int[] curr = pq.poll();
-            int cost = curr[0];
-            int sec = curr[1];
-            asnwer += cost;
-            if(head<=tail){
-                if(sec==0){
-                    pq.offer(new int[]{costs[head], 0});
-                    head++;
-                }else{
-                    pq.offer(new int[]{costs[tail], 1});
-                    tail--;
-                }
+            if(prod<k){
+                max += end-start+1;
             }
+            end++;
         }
-        return asnwer;
-
+        return max;
 
     }
 
