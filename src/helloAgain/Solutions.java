632a6177b4ae205dd413dcc7d4515188039d7aa1
dell <arjunpathak164@gmail.com>
2023-06-26 22:05:18 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -11,28 +11,46 @@ import java.util.*;
 
 
 public class Solutions {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int prod = 1;
-        int end = 0;
-        int start = 0;
+
+    public int orderOfLargestPlusSign(int n, int[][] mines) {
+        int[][] grid = new int[n][n];
+        for(int i =0 ;i<n;i++){
+            for(int j = 0;j<n;j++){
+                grid[i][j] = 1;
+            }
+        }
+        for(int[] i:mines){
+            grid[i[0]][i[1]] = 0;
+        }
         int max = 0;
-        while(end<nums.length){
-            prod *= nums[end];
-            while(prod>=k && start<end){
-                prod /= nums[start];
-                start++;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(grid[i][j]==1){
+                    max = Math.max(max, helper(grid, i, j));
+                }
             }
-            if(prod<k){
-                max += end-start+1;
-            }
-            end++;
         }
         return max;
-
     }
 
+    public int helper(int[][] grid, int i, int j){
 
+        int order = 0;
+        int left = 0, right = 0, up = 0, down = 0;
+        while(i-left>=0 && grid[i-left][j]==1){
+            left++;
+        }
+        while(i+ right<grid.length && grid[i+right][j]==1){
+            right++;
+        }
+        while(j-up>=0 && grid[i][j-up]==1)
+            up++;
+        while(j+down<grid.length && grid[i][j+down]==1){
+            down++;
+        }
+        return order = Math.min(down, Math.min(up, Math.min(left, right)));
 
+    }
 
     public static void main(String[] args){
         int[] nums = {0, -1};
