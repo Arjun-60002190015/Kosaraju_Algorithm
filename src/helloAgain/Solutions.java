54821121aad204cb6603cddfd729b6814c5fc926
dell <arjunpathak164@gmail.com>
2023-06-28 21:51:46 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -11,32 +11,45 @@ import java.util.*;
 
 
 public class Solutions {
-    public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int[] right = new int[n];
-        int[] left = new int[n];
-        left[0] = -1;
-        right[n-1] = n;
-
-        for(int i = 1;i<n;i++){
-            int prev= i-1;
-            while(prev>=0 && heights[prev]>=heights[i]){
-                prev = left[prev];
+    public int maximalRectangle(char[][] matrix) {
+        int max = 0;
+        int[][] dp = new int[matrix.length+1][matrix[0].length+1];
+        for(int i = 0;i< matrix.length;i++){
+            for(int j = 0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0)
+                    dp[i][j] = 0;
+                else if(matrix[i][j]=='1'){
+                    dp[i][j] = i==0?1:dp[i-1][j] + 1;
+                }
             }
-            left[i] = prev;
-        }
-        for(int i=  n-2;i>=0;i--){
-            int prev=  i+1;
-            while(prev<n && heights[prev]>=heights[i]){
-                prev = right[prev];
-            }
-            right[i] = prev;
         }
 
-        int max= 0 ;
-        for(int i=  0;i<n;i++){
-            int curr = right[i] - left[i] - 1;
-            max = Math.max(curr*heights[i], max);
+        for(int[] d:dp){
+
+            int[] left = new int[d.length];
+            int[] right = new int[d.length];
+            left[0] = -1;
+            right[d.length-1] = d.length;
+            for(int i = 0;i< d.length;i++){
+                int prev = i-1;
+                while(prev>=0 && d[prev]>=d[i]){
+                    prev = left[prev];
+                }
+                left[i] = prev;
+            }
+
+            for(int i = d.length-2;i>=0;i--){
+                int prev = i+1;
+                while(prev<d.length && d[prev]>=d[i]){
+                    prev = right[prev];
+                }
+                right[i] = prev;
+            }
+
+            for(int i = 0;i<d.length;i++){
+                int diff = right[i] - left[i]-1;
+                max = Math.max(max, diff * d[i]);
+            }
         }
         return max;
 
