a04de5b55a33cc1b4953e087b7994e6297eaaba7
dell <arjunpathak164@gmail.com>
2023-07-01 14:41:59 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -11,28 +11,26 @@ import java.util.*;
 
 
 public class Solutions {
-    int res = Integer.MAX_VALUE;
-    public int distributeCookies(int[] cookies, int k) {
-        helper(cookies, 0, k, new int[k]);
-        return res;
+
+    public int numTilePossibilities(String tiles) {
+        HashSet<String> set = new HashSet<>();
+        boolean[] vis = new boolean[tiles.length()];
+        helper(tiles, "", set, vis);
+        return set.size()-1;
 
     }
 
-    public void helper(int[] cookies, int index, int k, int[] h){
-        if(index==cookies.length){
-            int max = 0;
-            for(int i:h){
-                max = Math.max(i, max);
+    public void helper(String tiles, String curr,  HashSet<String> set, boolean[] vis){
+        set.add(curr);
+
+        for(int i = 0;i<tiles.length();i++){
+            if(!vis[i]){
+                vis[i] = true;
+                helper(tiles, curr+ tiles.charAt(i), set, vis);
+                vis[i] = false;
             }
-            res = Math.min(res, max);
-            return;
         }
 
-        for(int i = 0;i<k;i++){
-            h[i] += cookies[index];
-            helper(cookies, index+1, k, h);
-            h[i] -= cookies[index];
-        }
     }
 
 
