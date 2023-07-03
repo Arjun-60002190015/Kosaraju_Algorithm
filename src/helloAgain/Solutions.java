04f1a24fe3740c22ee751a034c422ac574485804
dell <arjunpathak164@gmail.com>
2023-07-03 16:16:48 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -7,30 +7,24 @@ import java.util.*;
 
 
 public class Solutions {
-    StringBuilder sb = new StringBuilder();
-    public String getHappyString(int n, int k) {
-        List<String> res = new ArrayList<>();
-        helper(n, res, ' ');
-        Collections.sort(res);
-        if(res.size()>=k)
-            return res.get(k-1);
-        return "";
-
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        helper(0, candidates, target, new ArrayList<>());
+        return res;
 
     }
 
-    public void helper(int n, List<String> res, char prev){
-        if(sb.length()==n) {
-            res.add(sb.toString());
+    public void helper(int index, int[] nums, int target, List<Integer> temp){
+        if(target==0){
+            res.add(new ArrayList<>(temp));
             return;
         }
-        for(char ch = 'a';ch<='c';ch++){
-            if(ch!=prev){
-                sb.append(ch);
-                helper(n, res, ch );
-                sb.delete(sb.length()-1, sb.length());
-
-            }
+        if(index== nums.length || target<0)
+            return;
+        for(int i = index;i< nums.length;i++){
+            temp.add(nums[i]);
+            helper(i, nums, target-nums[i], temp);
+            temp.remove(temp.size()-1);
         }
     }
 
