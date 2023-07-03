2e0bb711fd6a5af1cc9572c6ab28d471913ea94d
dell <arjunpathak164@gmail.com>
2023-07-03 22:51:47 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -7,36 +7,15 @@ import java.util.*;
 
 
 public class Solutions {
-    public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        helper( 0, s, res, new ArrayList<>());
-        return res;
-    }
-
-    public void helper(int start, String s, List<List<String>> res, List<String> temp){
-        if(start==s.length()){
-            res.add(new ArrayList<>(temp));
-            return;
-        }
-        for(int i = start;i<s.length();i++){
-            if(isPalin(s, start, i)){
-                temp.add(s.substring(start, i+1));
-                helper(i+1, s, res, temp);
-                temp.remove(temp.size()-1);
-            }
-        }
-
-    }
-
-    public boolean isPalin(String s, int i, int j){
-
-        while(i<=j){
-            if(s.charAt(i)==s.charAt(j)){
-                i++;
-                j--;
-            }else return false;
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int dist = Math.abs(target[0]) + Math.abs(target[1]);
+        for(int[] i:ghosts){
+            if(Math.abs(i[0]-target[0]) + Math.abs(i[1]-target[1])<=dist)
+                return false;
         }
         return true;
+
+
     }
 
     public static void main(String[] args){
