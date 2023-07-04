30da274d57c3ee08323f5b6c18b5c7ac903a5db3
dell <arjunpathak164@gmail.com>
2023-07-04 19:44:56 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -7,28 +7,32 @@ import java.util.*;
 
 
 public class Solutions {
-    boolean[] vis;
-    int ans = 0;
-
-    public int countArrangement(int n) {
-        vis = new boolean[n+1];
-        helper(n, 1);
-        return ans;
-    }
-
-    public void helper(int n, int index){
-        if(index>n) {
-            ans++;
-            return;
-        }
-
-        for(int i = 1;i<=n;i++){
-            if(!vis[i] && (i%index==0 || index%i==0)){
-                vis[i] = true;
-                helper(n, index+1);
-                vis[i] = false;
+    public int getMaximumGold(int[][] grid) {
+        int max = 0;
+        for(int i = 0;i< grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
+                max = Math.max(helper(grid, i, j), max);
             }
         }
+        return max;
+
+    }
+
+    public int helper(int[][] grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>= grid[0].length || grid[i][j]==0)
+            return 0;
+        int c = grid[i][j];
+        grid[i][j] = 0;
+        int max1 = 0;
+        int max = c;
+        max1 = Math.max(helper(grid, i+1, j), max1);
+        max1 = Math.max(helper(grid, i-1, j), max1);
+        max1 = Math.max(helper(grid, i, j+1), max1);
+        max1 = Math.max(helper(grid, i, j-1), max1);
+        grid[i][j] = c;
+        max += max1;
+        return max;
+
     }
 
 
