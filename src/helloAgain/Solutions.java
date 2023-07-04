1237641698d57dd466598b647b9b67e9dcce584d
dell <arjunpathak164@gmail.com>
2023-07-04 22:07:44 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -7,33 +7,32 @@ import java.util.*;
 
 
 public class Solutions {
-    public int getMaximumGold(int[][] grid) {
-        int max = 0;
-        for(int i = 0;i< grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                max = Math.max(helper(grid, i, j), max);
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> res = new HashSet<List<Integer>>();
+        helper(res, 0, Integer.MIN_VALUE, nums, new ArrayList<>());
+        List r = new ArrayList<>(res);
+        return r;
+    }
+
+    public void helper(Set<List<Integer>> res, int index, int prev, int[] nums, List<Integer> temp){
+        if(temp.size()>1){
+
+            res.add(new ArrayList<>(temp));
+
+        }
+
+
+
+        for(int i = index;i< nums.length;i++){
+            if(nums[i]>=prev){
+                temp.add(nums[i]);
+                helper(res, i+1, nums[i],nums, temp );
+                temp.remove(temp.size()-1);
             }
         }
-        return max;
-
     }
 
-    public int helper(int[][] grid, int i, int j){
-        if(i<0 || i>=grid.length || j<0 || j>= grid[0].length || grid[i][j]==0)
-            return 0;
-        int c = grid[i][j];
-        grid[i][j] = 0;
-        int max1 = 0;
-        int max = c;
-        max1 = Math.max(helper(grid, i+1, j), max1);
-        max1 = Math.max(helper(grid, i-1, j), max1);
-        max1 = Math.max(helper(grid, i, j+1), max1);
-        max1 = Math.max(helper(grid, i, j-1), max1);
-        grid[i][j] = c;
-        max += max1;
-        return max;
 
-    }
 
 
     public static void main(String[] args){
