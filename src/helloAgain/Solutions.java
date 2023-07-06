b991cd1489f51dd1e0997f982120cfbb11a29219
dell <arjunpathak164@gmail.com>
2023-07-06 19:58:24 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -7,27 +7,70 @@ import java.util.*;
 
 
 public class Solutions {
-    int max  = 0;
+    //Recursion Solution -
+    int max = 0;
 
-    public int maxUniqueSplit(String s) {
-        helper(0, s, new HashSet<String>());
+    public int maxProductRecursion(String s) {
+        char[] c = s.toCharArray();
+        helper(c, 0, "", "");
         return max;
     }
 
-    public void helper(int start, String s, HashSet<String> set){
-        if(start==s.length()){
-            max = Math.max(set.size(), max);
-            return;
-        }
-
-        for(int i = start+1; i<=s.length();i++){
-            String newS = s.substring(start, i);
-            if(!set.contains(newS)){
-                set.add(newS);
-                helper(i, s, set);
-                set.remove(newS);
+    public void helper(char[] c, int i, String s1, String s2){
+        if(i>=c.length){
+            if(isPalin(s1) && isPalin(s2)){
+                max = Math.max(max, s1.length()*s2.length());
+                return;
             }
         }
+
+        helper(c, i+1, s1+c[i], s2);
+        helper(c, i+1, s1, s2+c[i]);
+        helper(c, i+1, s1, s2);
+    }
+
+    public boolean isPalin(String s){
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    //Memoization -
+    int ans = 0;
+
+    Integer[][] dp;
+
+    public int maxProduct(String s) {
+        dp = new Integer[s.length()+1][s.length()+1];
+        char[] c = s.toCharArray();
+        helperM(0, c, "", "");
+        return ans;
+
+    }
+
+    public int helperM(int i, char[] c, String s1, String s2){
+
+        if(isPalin(s1) && isPalin(s2)){
+                ans = Math.max(ans, s1.length()*s2.length());
+        }
+
+        if(i>=c.length){
+            return dp[s1.length()][s2.length()];
+        }
+
+        dp[s1.length()][s2.length()] = ans;
+        int one = helperM(i+1, c, s1+c[i], s2);
+        int two = helperM(i+1, c, s1, s2+c[i]);
+        int three = helperM(i+1, c, s1, s2);
+        return dp[s1.length()][s2.length()];
+
     }
 
 
