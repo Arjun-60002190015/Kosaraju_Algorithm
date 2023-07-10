1921fbfbef2cfb860eced1b37817d3b605695be5
dell <arjunpathak164@gmail.com>
2023-07-10 16:17:48 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -7,13 +7,15 @@ import java.util.*;
 
 
 public class Solutions {
-    public int findValueOfPartition(int[] nums) {
+    public int minPairSum(int[] nums) {
         Arrays.sort(nums);
-        int min = Integer.MAX_VALUE;
-        for(int i = 1;i< nums.length;i++){
-            min = Math.min(nums[i]-nums[i-1], min);
+        int max = 0;
+        int end = nums.length-1;
+        for(int i = 0;i< nums.length;i++){
+            max = Math.max(max, nums[i]+nums[end]);
+            end--;
         }
-        return min;
+        return max;
 
     }
 
