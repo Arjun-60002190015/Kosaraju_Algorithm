2502bc3fcf0d0781272f34a68dd7eb1d29d86b28
dell <arjunpathak164@gmail.com>
2023-07-12 20:54:11 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -7,36 +7,15 @@ import java.util.*;
 
 
 public class Solutions {
-    public boolean canPartitionKSubsets(int[] nums, int k) {
-        int sum = 0;
-        for(int i:nums){
-            sum += i;
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long sum = mass;
+        for(int i = 0;i<asteroids.length;i++){
+            if(sum<asteroids[i]) return false;
+            sum += asteroids[i];
         }
-        if(sum%k!=0)
-            return false;
-        boolean[] vis = new boolean[nums.length];
-        Arrays.fill(vis, false);
-        boolean flag = false;
-        flag = helper(nums, sum/k, 0, vis, 0, k);
-        return flag;
-    }
+        return true;
 
-    public boolean helper(int[] nums, int limit, int index, boolean[] vis, int curr, int k){
-        if(curr==limit){
-            return helper(nums, limit, 0, vis, 0, k-1);
-        }
-
-        if(k==1)
-            return true;
-
-        for(int i = index;i< nums.length;i++){
-            if(vis[i] || curr+nums[i]>limit) continue;
-            vis[i] = true;
-            if(helper(nums, limit, i+1, vis, curr+nums[i], k))
-                return true;
-            vis[i] = false;
-        }
-        return false;
     }
 
     public static void main(String[] args){
