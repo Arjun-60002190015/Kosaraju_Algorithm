fe9483441274e6b6bf320ec490780bd61df03a16
dell <arjunpathak164@gmail.com>
2023-07-18 17:32:40 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -10,19 +10,36 @@ import java.util.*;
 
 public class Solutions {
 
-    public int longestArithSeqLength(int[] nums) {
-        Map<Integer, Integer>[] map = new HashMap[nums.length];
-        int count = 2;
-        for(int i = 0;i< nums.length;i++){
-            map[i] = new HashMap<>();
-            for(int j = 0;j<i;j++){
-                int diff = nums[i] - nums[j];
-                map[i].put(diff, map[j].getOrDefault(diff, 1)+1);
-                count = Math.max(count, map[i].get(diff));
+    public int getCommon(int[] nums1, int[] nums2) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 0;i<nums1.length;i++){
+            if(helper(nums2, nums1[i])) {
+                min = Math.min(min, nums1[i]);
+                break;
             }
         }
-        return count;
+        for(int i = 0;i<nums2.length;i++){
+            if(helper(nums1, nums2[i])) {
+                min = Math.min(min, nums2[i]);
+                break;
+            }
+        }
+        return min==Integer.MAX_VALUE?-1:min;
 
+    }
+
+    public boolean helper(int[] nums, int target){
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start+ (end-start)/2;
+            if(nums[mid]>target){
+                end = mid-1;
+            }else if(nums[mid]<target){
+                start = mid+1;
+            }else return true;
+        }
+        return false;
     }
 
 
