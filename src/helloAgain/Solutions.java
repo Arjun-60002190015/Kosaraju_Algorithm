55e87c13a7df903445468fcfff8df1936b64687a
dell <arjunpathak164@gmail.com>
2023-07-19 17:50:06 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,17 +9,36 @@ import java.util.*;
 
 
 public class Solutions {
-    public int singleNonDuplicate(int[] nums) {
-        int start = 0;
-        int end = nums.length-1;
-        while(start<end){
-            int mid = start + (end-start)/2;
-            if(mid%2==0 && nums[mid]!=nums[mid+1] || mid%2!=0 && nums[mid]!=nums[mid-1])
-                end = mid;
-            else start = mid+1;
+    public int maxDistance(int[] position, int m) {
+        Arrays.sort(position);
+        int start = Integer.MAX_VALUE;
+        int end = position[position.length-1]-position[0];
+        for(int i = 1;i<position.length;i++){
+            start = Math.min(start, position[i]-position[i-1]);
         }
-        return nums[start];
 
+        int ans = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(helper(mid, position, m)){
+                ans = mid;
+                start = mid+1;
+            }else end = mid-1;
+        }
+        return ans;
+
+    }
+
+    public boolean helper(int max, int[] position, int m){
+        int count = 1;
+        int prev = position[0];
+        for(int i = 1;i<position.length;i++){
+            if(position[i]-prev>=max){
+                prev = position[i];
+                count++;
+            }
+        }
+        return count>=m;
     }
 
 
