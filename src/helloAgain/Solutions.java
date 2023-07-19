ebde3586bc69cda52c8c033c2d2f3d284ce8cef5
dell <arjunpathak164@gmail.com>
2023-07-19 13:45:03 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,20 +9,17 @@ import java.util.*;
 
 
 public class Solutions {
-    public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1]-o2[1];
+    public int findMinArrowShots(int[][] points) {
+        int count = 1;
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+        int prev = points[0][1];
+        for(int i = 1;i< points.length;i++){
+            if(points[i][0]<=prev){
+                continue;
+            }else {
+                prev = points[i][1];
+                count++;
             }
-        });
-
-        int count = 0;
-        int end = -100000;
-        for(int[] i:intervals){
-            if(i[0]>=end)
-                end = i[1];
-            else count++;
         }
         return count;
 
