1128c3c19c7f840cc61452a3451f9759219f89ac
dell <arjunpathak164@gmail.com>
2023-07-19 13:24:33 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -9,40 +9,23 @@ import java.util.*;
 
 
 public class Solutions {
-    public int shipWithinDays(int[] weights, int days) {
-        int start = 0;
-        int end = 0;
-        for(int i:weights){
-            start = Math.max(start, i);
-            end += i;
-        }
-
-
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(helper(weights, mid, days)){
-                end = mid-1;
-            }else
-                start = mid + 1;
-        }
-        return start;
-    }
-
-    public boolean helper(int[] weights, int target, int days){
-        int count = 1;
-        int sum =  0;
-        for(int i = 0;i<weights.length;i++){
-            if(count>days) return false;
-            if(sum+weights[i]>target){
-                sum = 0;
-                count++;
-
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1]-o2[1];
             }
-            //else {
-                sum += weights[i];
+        });
 
+        int count = 0;
+        int end = -100000;
+        for(int[] i:intervals){
+            if(i[0]>=end)
+                end = i[1];
+            else count++;
         }
-        return count<=days;
+        return count;
+
     }
 
 
