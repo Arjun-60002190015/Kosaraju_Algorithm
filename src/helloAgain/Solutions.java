5151e12cc46404228c56fab1d3e2ac0136c5254a
dell <arjunpathak164@gmail.com>
2023-07-24 22:30:18 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -10,27 +10,17 @@ import java.util.*;
 
 
 public class Solutions {
-    public int[] executeInstructions(int n, int[] startPos, String s) {
-
-        int[] res = new int[s.length()];
-        for(int i = 0;i<s.length();i++){
-            res[i] = helper(startPos[0], startPos[1], s, i, n) - i;
+    public String shiftingLetters(String s, int[] shifts) {
+        char[] arr = s.toCharArray();
+        for(int i = shifts.length-1;i>0;i--){
+            shifts[i-1] += shifts[i];
+            shifts[i-1] %= 26;
         }
-        return res;
-    }
 
-    public int helper(int i, int j, String s, int index, int n){
-        if(i<0 || i>= n || j<0 || j>=n){
-            return index-1;
+        for(int i = 0;i< arr.length;i++){
+            arr[i] = (char)('a' + (arr[i]-'a' + shifts[i])%26);
         }
-        if(index>=s.length()) return index;
-        if(s.charAt(index)=='U')
-            return helper(i-1, j, s, index+1, n);
-        if(s.charAt(index)=='D')
-            return helper(i+1, j, s, index+1, n);
-        if(s.charAt(index)=='L')
-            return helper(i, j-1, s, index+1, n);
-        else return helper(i, j+1, s, index+1, n);
+        return String.valueOf(arr);
     }
     public static void main(String[] args){
 
