2a86ac54b0a490a43b03a89ec19ff72b36f8d596
dell <arjunpathak164@gmail.com>
2023-07-24 20:12:44 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -10,20 +10,31 @@ import java.util.*;
 
 
 public class Solutions {
-    public double myPow(double x, int n) {
-        if(n>=0){
-            return helper(x, n);
-        }else{
-            return 1/helper(x, n);
-        }
+    Integer[][][] dp;
+    int mod = (int)1e9+7;
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        dp = new Integer[m][n][maxMove+1];
+        int[][] grid = new int[m][n];
+        int ans = helper(grid, dp, maxMove, startRow, startColumn) % mod;
+        return ans;
+
     }
 
-    public double helper(double x, int n){
-        if(n==0) return 1;
-        double res = helper(x, n/2);
-        if(n%2!=0)
-            return res*res*x;
-        return res*res;
+    public int helper(int[][] grid, Integer[][][] dp, int k, int i, int j){
+        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length)
+            return 1;
+        if(k==0) return 0;
+        if(dp[i][j][k]!=null) return dp[i][j][k] % mod;
+
+        int max = 0;
+        max = (helper(grid, dp, k-1, i+1, j) % mod+
+                helper(grid, dp, k-1, i-1, j) % mod) % mod+
+                (helper(grid, dp, k-1, i, j+1)%mod+
+                        helper(grid, dp, k-1, i, j-1)% mod) % mod;
+        return dp[i][j][k] = (max%mod);
+    }
+    public static void main(String[] args){
+
     }
 
 }
