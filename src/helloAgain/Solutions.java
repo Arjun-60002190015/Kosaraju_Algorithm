dc3a6de4fbefb8d49799e9c46dd36c26797c7d00
dell <arjunpathak164@gmail.com>
2023-07-28 18:19:56 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -10,38 +10,33 @@ import java.util.*;
 
 
 public class Solutions {
+    int max = 0;
+    public int findMaxFish(int[][] grid) {
+        for(int i = 0;i< grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
+                if(grid[i][j]!=0){
+                    max = Math.max(max, helper(i, j, grid));
+                }
 
-    public long maxRunTime(int n, int[] batteries) {
-        long start = 1;
-        long end = 0;
-        for(int i:batteries){
-            end += i;
-        }
-        long ans = 0;
-        while(start<=end){
-            long mid = start + (end-start)/2;
-            if(helper(n, batteries, mid)){
-                ans = mid;
-                start = mid+1;
-            }else {
-                end = mid-1;
             }
         }
-        return ans;
-
+        return max;
     }
 
-    public boolean helper(int n, int[] batteries, long mid){
-        long sum = 0;
-        for(int i=  0;i< batteries.length;i++){
-            if(batteries[i]<=mid){
-                sum += batteries[i];
-            }else{
-                sum += mid;
-            }
-
+    public int helper(int i, int j, int[][] grid){
+        if(i<0 || j<0 || i>= grid.length || j>=grid[0].length || grid[i][j]==0){
+            return 0;
         }
-        return sum >=mid*n;
+        int curr = grid[i][j];
+        grid[i][j] = 0;
+
+        int up = helper(i+1, j, grid);
+        int down = helper(i-1, j, grid);
+        int right = helper(i, j+1, grid);
+        int left = helper(i, j-1, grid);
+        return curr + up+ down+left+right;
+
+
     }
 
     public static void main(String[] args){
