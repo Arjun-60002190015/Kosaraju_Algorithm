003b8e020dedcc24304b5838a8f105b8a3d6c5b2
dell <arjunpathak164@gmail.com>
2023-07-30 19:17:10 +0530

    kggfhfhyt

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -6,54 +6,41 @@ import java.util.*;
 
 
 public class Solutions {
-    Integer[][] dp;
-    int max = 1;
-    public int longestIncreasingPath(int[][] matrix) {
-
-        int m = matrix.length;
-        int n = matrix[0].length;
-        dp = new Integer[m][n];
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                int path =
-                        helper(matrix, i, j, -1);
-                max = Math.max(max, path);
+    public static String longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        for(int i = 0;i<text1.length();i++){
+            for(int j = 0;j<text2.length();j++){
+                if(text1.charAt(i)==text2.charAt(j)){
+                    dp[i+1][j+1] = 1 + dp[i][j];
+                }else{
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
             }
         }
-        return max;
-
-    }
-
-    public int helper(int[][] matrix, int i, int j, int prev){
-        if(i<0 || i>= matrix.length || j<0 || j>= matrix[0].length || matrix[i][j]<=prev)
-            return 0;
-        if(dp[i][j]!=null) return dp[i][j];
-        int max = 1;
-        int up = 1+
-                helper(matrix, i-1, j, matrix[i][j]);
-        int down = 1+
-                helper(matrix, i+1, j, matrix[i][j]);
-
-        int left =  1+helper(matrix, i, j-1,matrix[i][j]);
-        int right = 1+
-                helper(matrix, i, j+1, matrix[i][j]);
-        return dp[i][j] = Math.max(max,Math.max(up, Math.max(down, Math.max(left, right))));
-
+        StringBuilder res = new StringBuilder();
+        int i = text1.length();
+        int j = text2.length();
+        while(i>0 && j>0){
+            if(text1.charAt(i-1)==text2.charAt(j-1)){
+                res.append(text1.charAt(i-1));
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i][j-1]>dp[i-1][j]){
+                    j--;
+                }else{
+                    i--;
+                }
+            }
+        }
+        return res.reverse().toString();
     }
 
 
 
     public static void main(String[] args){
-        /*
-        Scanner sc = new Scanner(System.in);
-        int res = 0;
-        int t = sc.nextInt();
-        for(int i = 0;i<t;i++){
-            long n = sc.nextLong();
-            //System.out.println(helper(n));
-            }
-
-         */
+        System.out.println(longestCommonSubsequence(new String("bca"), new String("aaa")));
 
     }
 
