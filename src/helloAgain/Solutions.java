fceb312b3f68f5c5f11360304b8e7f3d374df441
dell <arjunpathak164@gmail.com>
2023-07-31 19:47:40 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -6,37 +6,25 @@ import java.util.*;
 
 
 public class Solutions {
-    public int minDistance(String word1, String word2) {
-        int lcs = helper(word1, word2);
-        return word1.length()+word2.length() - 2*lcs;
-
-    }
-
-    public static int helper(String s1, String s2){
-        int[][] dp = new int[s1.length()+1][s2.length()+1];
-        for(int i = 0;i<s1.length();i++){
-            for(int j = 0;j< s2.length();j++){
-                if(i==0 || j==0){
-                    dp[i][j] = 0;
-                }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s:strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String sorted = String.valueOf(c);
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
             }
+            map.get(sorted).add(s);
         }
-        for(int i = 1;i<=s1.length();i++){
-            for(int j = 1;j<=s2.length();j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return dp[s1.length()][s2.length()];
+        return new ArrayList<>(map.values());
+
     }
 
 
 
     public static void main(String[] args){
-        System.out.println(helper(new String("sea"), new String("eat")));
+        //System.out.println(helper(new String("sea"), new String("eat")));
 
     }
 
