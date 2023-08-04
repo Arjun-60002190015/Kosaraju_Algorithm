a5e9a1e69aeb42d72cb0c7ff472a9f2e67b2265c
dell <arjunpathak164@gmail.com>
2023-08-04 19:40:02 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -6,31 +6,29 @@ import java.util.*;
 
 
 public class Solutions {
-    public int minExtraChar(String s, String[] dictionary) {
-        Integer[] dp = new Integer[s.length()+1];
-        Set<String> set = new HashSet<>();
-        for(String j:dictionary) set.add(j);
-        return helper(s, set, 0, dp);
-        //return s.length()-taken;
-    }
-
-    public int helper(String s, Set<String> set, int index, Integer[] dp){
-        if(index==s.length()) return 0;
-        if(dp[index]!=null) return dp[index];
-        int min = Integer.MAX_VALUE;
-        for(int i = index+1;i<=s.length();i++){
-            if(set.contains(s.substring(index, i))){
-                min = Math.min(min, 0 + helper(s, set, i, dp));
-            }else{
-                min = Math.min(min, i-index+ helper(s, set, i, dp));
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> res = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch, i);
+        }
+        int max = 0;
+        int prev = -1;
+        for(int i = 0;i<s.length();i++){
+            max = Math.max(max, map.get(s.charAt(i)));
+            if(max==i){
+                res.add(max-prev);
+                prev = i;
             }
         }
-        return dp[index] = min;
+        return res;
+
     }
 
 
     public static void main(String[] args){
-        //System.out.println(pivotInteger(8));
+        //System.out.println(partitionString("abacaba"));
 
     }
 
