b3b047ff16a72708bf0619155c82933e7b7ddfdb
dell <arjunpathak164@gmail.com>
2023-08-07 22:03:06 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,44 +8,17 @@ import java.util.*;
 
 
 public class Solutions {
-    //Not Space Optimized
-    public int partitionDisjointMethod1(int[] nums) {
-        int n = nums.length;
-        int[] min = new int[n];
-        int[] max = new int[n];
-        max[0] = nums[0];
-        min[n-1] = nums[n-1];
-        for(int i = 1;i<n;i++){
-            max[i] = Math.max(nums[i], max[i-1]);
+    public long maxKelements(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i:nums) pq.add(i);
+        long sum = 0;
+        while(k-->0){
+            int num = pq.poll();
+            sum += num;
+            num = (int)Math.ceil(num/3.0);
+            pq.add(num);
         }
-        for(int i = n-2;i>=0;i--){
-            min[i] = Math.min(nums[i], min[i+1]);
-        }
-        int count = 0;
-        for(int i = n-1;i>=0;i--){
-            if(max[i]<min[i]){
-                count = Math.max(count, i+1);
-            }
-        }
-        return count;
-    }
-
-    //Space Optimized:
-    public int partitionDisjoint(int[] nums) {
-        int max = nums[0];
-        int leftMax = nums[0];
-        int n = nums.length;
-        int len = 0;
-        for(int i = 1;i<n;i++){
-            if(nums[i]>leftMax){
-                len = i+1;
-                leftMax = max;
-            }else{
-                max = Math.max(max, nums[i]);
-            }
-        }
-        return len;
-
+        return sum;
 
     }
 
@@ -53,7 +26,13 @@ public class Solutions {
 
 
     public static void main(String[] args){
-        //System.out.println(partitionString("abacaba"));
+        List<Integer> res = new ArrayList<>();
+        res.add(2);
+        res.add(3);
+        res.add(3);
+        res.add(2);
+        res.add(3);
+        //System.out.println(canSplitArray(res, 6));
 
     }
 
