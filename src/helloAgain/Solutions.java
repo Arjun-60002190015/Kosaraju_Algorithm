83bcc16bfc1ac39fcc8a45ecd35a8a3d4c5eeb60
dell <arjunpathak164@gmail.com>
2023-08-13 13:28:48 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,18 +8,48 @@ import java.util.*;
 
 
 public class Solutions {
-    public int minAbsoluteDifference(List<Integer> nums, int x) {
-        TreeSet<Integer> set = new TreeSet<>();
-        int min = Integer.MAX_VALUE;
-        for(int i = x;i<nums.size();i++){
-            set.add(nums.get(i-x));
-            Integer ceil = set.ceiling(nums.get(i));
-            if(ceil!=null) min = Math.min(min, Math.abs(ceil - nums.get(i)));
-            Integer floor = set.floor(nums.get(i));
-            if(floor!=null) min = Math.min(min, Math.abs(floor- nums.get(i)));
-        }
-        return min;
+    //Recursion
+    public boolean validPartitionRecursion(int[] nums) {
 
+        return helper(nums, 0);
+    }
+
+    public boolean helper(int[] nums, int index){
+        if(index>= nums.length) return true;
+        if(index<= nums.length-3 && nums[index]==nums[index+1] && nums[index]==nums[index+2]){
+            return helper(nums, index+2) || helper(nums, index+3);
+        }
+        if(index<=nums.length-2 && nums[index]==nums[index+1])
+            return helper(nums, index+2);
+        if(index<= nums.length-3){
+            if(nums[index]==nums[index+1]-1 && nums[index]==nums[index+2]-2)
+                return helper(nums, index+3);
+        }
+        return false;
+    }
+
+    //Memoization
+
+    public boolean validPartition(int[] nums) {
+        Boolean[] dp = new Boolean[nums.length];
+        return helper1(nums, 0, dp);
+    }
+    public boolean helper1(int[] nums, int i, Boolean[] dp){
+
+        if(i>= nums.length) return true;
+        if(dp[i]!=null) return dp[i];
+        if(i<= nums.length-3 && nums[i]==nums[i+1] && nums[i]==nums[i+2]){
+            return dp[i] = helper1(nums, i+2, dp) || helper1(nums, i+3, dp);
+        }
+        if(i<=nums.length-2 && nums[i]==nums[i+1]){
+            return dp[i] = helper1(nums, i+2, dp);
+        }
+        if(i<= nums.length-3){
+            if(nums[i]==nums[i+1]-1 && nums[i]==nums[i+2]-2){
+                return dp[i] = helper1(nums, i+3, dp);
+            }
+        }
+        return dp[i] = false;
     }
 
 
