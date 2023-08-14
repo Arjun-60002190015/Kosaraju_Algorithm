c625093eac34930e3253b93e3e17feea46d2f1f9
dell <arjunpathak164@gmail.com>
2023-08-14 19:08:07 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,35 +8,40 @@ import java.util.*;
 
 
 public class Solutions {
-    public List<Integer> topStudents(String[] positive_feedback, String[] negative_feedback, String[] report, int[] student_id, int k) {
-        HashSet<String> positive = new HashSet<>();
-        for(String i:positive_feedback){
-            positive.add(i);
-        }
-        HashSet<String> negative = new HashSet<>();
-        for(String i:negative_feedback){
-            negative.add(i);
-        }
-        //PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int[][] arr2 = new int[report.length][2];
-        for(int i = 0;i< report.length;i++){
-            String[] arr = report[i].split(" ");
-            int score = 0;
-            for(String n:arr){
-                if(positive.contains(n)) score += 3;
-                if(negative.contains(n)) score -= 1;
+    public int uniquePathsIII(int[][] grid) {
+        //Integer[][] dp = new Integer[grid.length][grid[0].length];
+        int count = 0;
+        int x = -1;
+        int y = -1;
+        int zeroes = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) {
+                    zeroes++;
+                }
+                if(grid[i][j]==1){
+                    x = i;y = j;
+                }
             }
-            arr2[i][0] = student_id[i];
-            arr2[i][1] = score;
         }
-        Arrays.sort(arr2, (a,b)-> a[1]==b[1]? Integer.compare(a[0], b[0]):Integer.compare(b[1], a[1]));
-        List<Integer> res = new ArrayList<>();
-        for(int i = 0;i<k;i++){
-            res.add(arr2[i][0]);
-        }
-        return res;
+        count += helper(x, y, grid, zeroes);
+        return count;
+    }
 
+    public int helper(int i, int j, int[][] grid, int k){
+        if(i<0 || j<0 || i>= grid.length || j>=grid[0].length || grid[i][j]==-1)
+            return 0;
+        if(grid[i][j]==2) return k==-1?1:0;
+        grid[i][j] = -1;
+        k--;
+        int path = helper(i+1, j, grid, k)+
+                helper(i, j+1, grid, k)+
+                helper(i-1, j, grid, k)+
+                helper(i, j-1, grid, k);
 
+        grid[i][j] = 0;
+        k++;
+        return path;
     }
 
 
