9acaee0e44247e0d70b3986abfea9a348d5e7db6
dell <arjunpathak164@gmail.com>
2023-08-14 18:15:19 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,38 +8,35 @@ import java.util.*;
 
 
 public class Solutions {
-    Random rnd = new Random();
-    public int findKthLargest(int[] nums, int k) {
-        int index = nums.length-k;
-        int start = 0;
-        int end = nums.length-1;
-        while(start<end){
-            int pivot = partition(nums, start, end);
-            if(pivot==index) break;
-            else if(pivot<index) start = pivot+1;
-            else end = pivot-1;
+    public List<Integer> topStudents(String[] positive_feedback, String[] negative_feedback, String[] report, int[] student_id, int k) {
+        HashSet<String> positive = new HashSet<>();
+        for(String i:positive_feedback){
+            positive.add(i);
         }
-        return nums[index];
-
-    }
-
-    public int partition(int[] nums, int start, int end){
-        swap(nums, end, start + rnd.nextInt(end-start));
-        int pivot = nums[end];
-        int j = start;
-        for(int i = start;i<end;i++){
-            if(nums[i]<pivot){
-                swap(nums, i, j++);
+        HashSet<String> negative = new HashSet<>();
+        for(String i:negative_feedback){
+            negative.add(i);
+        }
+        //PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[][] arr2 = new int[report.length][2];
+        for(int i = 0;i< report.length;i++){
+            String[] arr = report[i].split(" ");
+            int score = 0;
+            for(String n:arr){
+                if(positive.contains(n)) score += 3;
+                if(negative.contains(n)) score -= 1;
             }
+            arr2[i][0] = student_id[i];
+            arr2[i][1] = score;
         }
-        swap(nums, end, j);
-        return j;
-    }
+        Arrays.sort(arr2, (a,b)-> a[1]==b[1]? Integer.compare(a[0], b[0]):Integer.compare(b[1], a[1]));
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0;i<k;i++){
+            res.add(arr2[i][0]);
+        }
+        return res;
 
-    public void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+
     }
 
 
