af8221b72991ba88975ee426f0fee57f2272de64
dell <arjunpathak164@gmail.com>
2023-08-16 00:49:36 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,40 +8,22 @@ import java.util.*;
 
 
 public class Solutions {
-    public int uniquePathsIII(int[][] grid) {
-        //Integer[][] dp = new Integer[grid.length][grid[0].length];
+
+    public int findPairs(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> rep = new HashSet<>();
         int count = 0;
-        int x = -1;
-        int y = -1;
-        int zeroes = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 0) {
-                    zeroes++;
-                }
-                if(grid[i][j]==1){
-                    x = i;y = j;
-                }
-            }
+        for(int i:nums)
+            if(set.contains(i)) rep.add(i);
+            else set.add(i);
+        for(int i:set){
+
+            if(set.contains(i-k)) count++;
+            //if(set.contains(nums[i]+k)) count++;
         }
-        count += helper(x, y, grid, zeroes);
+        if(k==0) return rep.size();
         return count;
-    }
 
-    public int helper(int i, int j, int[][] grid, int k){
-        if(i<0 || j<0 || i>= grid.length || j>=grid[0].length || grid[i][j]==-1)
-            return 0;
-        if(grid[i][j]==2) return k==-1?1:0;
-        grid[i][j] = -1;
-        k--;
-        int path = helper(i+1, j, grid, k)+
-                helper(i, j+1, grid, k)+
-                helper(i-1, j, grid, k)+
-                helper(i, j-1, grid, k);
-
-        grid[i][j] = 0;
-        k++;
-        return path;
     }
 
 
