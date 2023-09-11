2e4bf7fbc9a7c571b99129df7142f52e7a6b8bda
dell <arjunpathak164@gmail.com>
2023-09-11 14:51:54 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -8,35 +8,39 @@ import java.util.*;
 
 
 public class Solutions {
-    int mod = (int)1e9+7;
-    HashMap<String, Integer> map;
-    public int numWays(int steps, int arrLen) {
-        //Integer[][] dp = new Integer[arrLen+1][steps+1];
-        map = new HashMap<>();
-        return helper(0, steps, arrLen);
 
-    }
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
 
-    public int helper(int i, int k, int len){
-        if(i<0 || i>=len) return 0;
-        if(k==0){
-            return i==0?1:0;
+        int[] col = new int[n];
+        Arrays.fill(col, -1);
+
+        for(int i = 0;i<n;i++){
+            if(col[i]==-1){
+                if(!helper(graph, i, 0, col)) return false;
+            }
         }
-        String res = i+","+k;
-        if(map.containsKey(res)) return map.get(res);
-        int ways = 0;
-        ways = (ways+ helper(i-1, k-1, len))%mod;
-        ways = (ways + helper(i+1, k-1, len))%mod;
-        ways = (ways + helper(i, k-1, len))%mod;
-        map.put(res, ways);
-        return ways;
+        return true;
+
     }
 
+    public boolean helper(int[][] graph, int i, int color, int[] col){
+        col[i] = color;
+
+        for(int j:graph[i]) {
+            if(col[j]==-1){
+                if(!helper(graph, j, 1-color, col)) return false;
+            }else if(col[j]==color) return false;
+        }
+        return true;
+
+    }
 
 
 
 
     public static void main(String[] args){
+        //int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
         int[] nums = {3, 4, 9, 1, 3, 9, 5};
         //System.out.println(canBeEqual("cmpr", "rmcp"));
     }
