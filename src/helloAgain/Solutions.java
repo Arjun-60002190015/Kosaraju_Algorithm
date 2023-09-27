98b092a42dc3821d3f33626c20188a94c8c4bcf4
dell <arjunpathak164@gmail.com>
2023-09-27 21:10:22 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -2,6 +2,7 @@ package helloAgain;
 
 
 import com.sun.java.swing.plaf.windows.WindowsDesktopIconUI;
+import javafx.util.Pair;
 import netscape.security.UserTarget;
 
 import java.util.*;
@@ -10,36 +11,47 @@ import java.util.*;
 
 public class Solutions {
 
-    public int[] shortestPath(int[][] edges, int n, int m, int src) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0;i<n;i++) graph.add(new ArrayList<>());
-        for(int i= 0;i<m;i++){
-            graph.get(edges[i][0]).add(edges[i][1]);
-            graph.get(edges[i][1]).add(edges[i][0]);
-        }
+    int[][] dist = new int[105][105];
+    int[] dx = {0, 1, -1, 0};
+    int[] dy = {1, 0, 0, -1};
 
-        int[] dist = new int[n];
-        for(int i = 0;i<n;i++){
-            dist[i] = (int)1e9;
+    public int minimumEffortPath(int[][] heights) {
+        int rows = heights.length;
+        int cols = heights[0].length;
+        for(int i = 0;i<rows;i++){
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
-        dist[src] = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(src);
-        while(!q.isEmpty()){
-            int node = q.poll();
-            for(int i:graph.get(node)){
-                if(dist[node] + 1<dist[i]){
-                    dist[i] = dist[node]+1;
-                    q.add(i);
+        return helper(heights);
+
+    }
+
+    public int helper(int[][] heights){
+        int rows = heights.length;
+        int cols = heights[0].length;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(-a[0], -b[0]));
+        pq.add(new int[]{0, 0, 0});
+        dist[0][0] = 0;
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int cost = -curr[0];
+            int x = curr[1];
+            int y = curr[2];
+            if(cost>dist[x][y]) continue;
+            if(x==rows-1 && y==cols-1) return cost;
+            for(int i = 0;i<4;i++){
+                int newx = x + dx[i];
+                int newy = y + dy[i];
+                if(newx<0 || newx>=rows || newy<0 || newy>=cols) continue;
+                int newdist = Math.max(dist[x][y], Math.abs(heights[x][y] - heights[newx][newy]));
+                if(newdist<dist[newx][newy]){
+                    dist[newx][newy] = newdist;
+                    pq.add(new int[]{-newdist, newx, newy});
                 }
             }
-        }
-        //below code only to return -1:
-        for(int i = 0;i<n;i++){
-            if(dist[i]==1e9) dist[i] = -1;
-        }
-        return dist;
 
+        }
+        return dist[rows-1][cols-1];
 
     }
 
