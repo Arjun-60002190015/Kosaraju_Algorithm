cd8e98bf17d14e19a72085aea4f03f4764cf6e29
dell <arjunpathak164@gmail.com>
2023-10-04 16:51:04 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -11,48 +11,33 @@ import java.util.*;
 
 public class Solutions {
 
-    int[][] dist = new int[105][105];
-    int[] dx = {0, 1, -1, 0};
-    int[] dy = {1, 0, 0, -1};
-
-    public int minimumEffortPath(int[][] heights) {
-        int rows = heights.length;
-        int cols = heights[0].length;
-        for(int i = 0;i<rows;i++){
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        return helper(heights);
-
-    }
-
-    public int helper(int[][] heights){
-        int rows = heights.length;
-        int cols = heights[0].length;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(-a[0], -b[0]));
-        pq.add(new int[]{0, 0, 0});
-        dist[0][0] = 0;
-        while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int cost = -curr[0];
-            int x = curr[1];
-            int y = curr[2];
-            if(cost>dist[x][y]) continue;
-            if(x==rows-1 && y==cols-1) return cost;
-            for(int i = 0;i<4;i++){
-                int newx = x + dx[i];
-                int newy = y + dy[i];
-                if(newx<0 || newx>=rows || newy<0 || newy>=cols) continue;
-                int newdist = Math.max(dist[x][y], Math.abs(heights[x][y] - heights[newx][newy]));
-                if(newdist<dist[newx][newy]){
-                    dist[newx][newy] = newdist;
-                    pq.add(new int[]{-newdist, newx, newy});
+    public int[] bellman_ford(int n, int[][] graph, int s){
+        int[] dist = new int[n];
+        for(int i = 0;i<n;i++) dist[i] = (int)1e8;
+        dist[s] = 0;
+        //Relaxation for n-1 times -
+        for(int i = 0;i<n-1;i++){
+            for(int[] j:graph){
+                int u = j[0];
+                int v = j[1];
+                int wt = j[2];
+                if(dist[u]!=(int)1e8 && dist[u] + wt < dist[v]){
+                    dist[v] = dist[u] + wt;
                 }
+
             }
-
         }
-        return dist[rows-1][cols-1];
-
+        //Relaxation now for n-th iteration and if value further reduces then there's
+        //Negative Cycle
+        for(int[] j:graph){
+            int u = j[0];
+            int v = j[1];
+            int wt = j[2];
+            if(dist[u]!=(int)1e8 && dist[u] + wt < dist[v]){
+                return new int[]{-1};
+            }
+        }
+        return dist;
     }
 
 
@@ -60,8 +45,12 @@ public class Solutions {
 
     public static void main(String[] args){
         //int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        int[][] nums = {{2}, {3},  {4, 5}, {2}, {}};
-        //System.out.println(kahnAlgo(nums));
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(2);
+
+        //System.out.println(winnerOfGame(new String("AAABABB")));
     }
 
 }
