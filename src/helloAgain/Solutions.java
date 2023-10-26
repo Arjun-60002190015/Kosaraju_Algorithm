cd5c80c2e00e5fc13cef6596a1d5b0cc800fe815
dell <arjunpathak164@gmail.com>
2023-10-26 19:16:19 +0530

    WD

--- src/helloAgain/Solutions.java
+++ src/helloAgain/Solutions.java
@@ -2,6 +2,7 @@ package helloAgain;
 
 
 import com.sun.java.swing.plaf.windows.WindowsDesktopIconUI;
+import com.sun.javafx.image.IntPixelGetter;
 import javafx.util.Pair;
 import netscape.security.UserTarget;
 
@@ -10,104 +11,55 @@ import java.util.*;
 
 
 public class Solutions {
-
-    //Dijkstra Algo:
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
-        for(int[] edge:edges){
-            if(!graph.keySet().contains(edge[0]))
-                graph.put(edge[0], new HashMap<>());
-            graph.get(edge[0]).put(edge[1], edge[2]);
-            if(!graph.keySet().contains(edge[1]))
-                graph.put(edge[1], new HashMap<>());
-            graph.get(edge[1]).put(edge[0], edge[2]);
+    //Function to find number of strongly connected components in the graph:
+    public int kosaraju(int v, List<List<Integer>> graph){
+        int[] vis = new int[v];
+        Stack<Integer> stack = new Stack<>();
+        //Ordering and putting in the Stack:
+        for(int i = 0;i<v;i++){
+            if(vis[i]==0) dfs(i, vis, graph, stack);
         }
-
-        int min = Integer.MAX_VALUE;
-        int res = -1;
-
-        for(int i = 0;i<n;i++){
-            Queue<int[]> pq = new PriorityQueue<>((a,b) -> (Integer.compare(a[1], b[1])));
-            boolean[] vis = new boolean[n];
-            int count = 0;
-            pq.add(new int[]{i, 0});
-            while(!pq.isEmpty()){
-                int[] curr = pq.poll();
-                int node = curr[0];
-                int dis = curr[1];
-
-                if(vis[node]) continue;
-                vis[node] = true;
-                count++;
-                if(graph.keySet().contains(node)){
-                    for(int adj:graph.get(node).keySet()){
-                        if(!vis[adj] && dis + graph.get(node).get(adj) <= distanceThreshold)
-                            pq.add(new int[] {adj, dis + graph.get(node).get(adj)});
-                    }
-                }
+        //Reversing the connections:
+        List<List<Integer>> grap = new ArrayList<>();
+        for(int i = 0;i<v;i++){
+            vis[i] = 0;
+            for(Integer j: grap.get(i)){
+                grap.get(j).add(i);
             }
-            if(count-1 <=min){
-                min = count-1;
-                res = i;
-            }
-
-
         }
-        return res;
-
-
+        //Counting the SCCs:
+        int scc = 0;
+        while(!stack.isEmpty()){
+            int node = stack.peek();
+            stack.pop();
+            if(vis[node]==0){
+                scc++;
+                helper2(node, vis, grap);
+            }
+        }
+        return scc;
     }
-    // Floyd Warshall Algo:
 
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] dist = new int[n][n];
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                dist[i][j] = Integer.MAX_VALUE;
-            }
+    public void dfs(int node, int[] vis, List<List<Integer>> graph, Stack<Integer> stack){
+        vis[node] = 1;
+        for(Integer j:graph.get(node)){
+            if(vis[j]==0) dfs(j, vis, graph, stack);
         }
-        for(int i = edges.length;i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int wt = edges[i][2];
-            dist[u][v] = wt;
-            dist[v][u] = wt;
-        }
-        for(int i = 0;i<n;i++) dist[i][i] = 0;
-        for(int k = 0;k<n;k++){
-            for(int i = 0;i<n;i++){
-                for(int j = 0;j<n;j++){
-                    if(dist[i][k]==Integer.MAX_VALUE || dist[k][j]==Integer.MAX_VALUE) continue;
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
-            }
+        stack.push(node);
+    }
 
+    public void helper2(int node, int[] vis, List<List<Integer>> graph){
+        vis[node] = 1;
+        for(Integer j:graph.get(node)){
+            if(vis[j]==0) helper2(j, vis, graph);
         }
-        int count = n;
-        int max = -1;
-        for(int i = 0;i<n;i++){
-            int temp = 0;
-            for(int j = 0;j<n;j++){
-                if(dist[i][j]<=distanceThreshold){
-                    temp++;
-                }
-            }
-            if(temp<=count){
-                count = temp;
-                max = i;
-            }
-        }
-        return max;
-
     }
 
 
 
 
     public static void main(String[] args){
-
-
-        System.out.println(getPhoneNumber(new String("five one zero two four eight zero double three two")));
+        //System.out.println(backspaceCompare(new String("ab#c"), new String("ad#c")));
     }
 
 }
